8432ae7d2af33c1b118e40123a4f190af01646c0
Alexandre Annic <alexandre.annic1@gmail.com>
2022-03-16 16:28:04 +0100

    Fix import next/script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,7 +17,6 @@ import * as React from 'react'
 import Document, {Head, Html, Main, NextScript} from 'next/document'
 import createEmotionServer from '@emotion/server/create-instance'
 import createEmotionCache from 'core/createEmotionCache'
-import Script from 'next/script'
 
 export default class MyDocument extends Document {
   render() {
