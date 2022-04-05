42754aa9bb215af6383967c40492ad1480aea104
Alexandre Annic <alexandre.annic1@gmail.com>
2022-04-05 07:18:30 +0200

    Add 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,31 @@
+import {Page} from '../shared/Page/Page'
+import {Fender} from 'mui-extension'
+import {useI18n} from '../core/i18n'
+import {ScButton} from '../shared/Button/Button'
+import {siteMap} from '../core/siteMap'
+import Link from 'next/link'
+import {Box} from '@mui/material'
+
+const NotFound = () => {
+  const {m} = useI18n()
+  return (
+    <Page>
+      <Fender
+        icon="pan_tool"
+        // icon="travel_explore"
+        title={m.pageNotFoundTitle}
+        description={<Box sx={{mt: 1}} dangerouslySetInnerHTML={{__html: m.pageNotFoundDesc}}/>}
+      >
+        <Link href={siteMap.index}>
+          <ScButton icon="home" variant="contained" sx={{
+            display: 'block',
+            margin: 'auto',
+            mt: 3
+          }}>{m.backToHome}</ScButton>
+        </Link>
+      </Fender>
+    </Page>
+  )
+}
+
+export default NotFound
