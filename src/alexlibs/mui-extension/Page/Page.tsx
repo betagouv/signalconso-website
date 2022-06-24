132cd5697da7925eea8a8bac8d67dc8801719edb
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2022-06-24 14:41:19 +0200

    fix some issues

--- src/alexlibs/mui-extension/Page/Page.tsx
+++ src/alexlibs/mui-extension/Page/Page.tsx
@@ -10,14 +10,16 @@ export interface PageProps extends BoxProps {
   children: ReactNode
 }
 
-let timeout
+let timeout: NodeJS.Timeout | undefined
 
 const Page = ({children, width, sx, animated = true, ...props}: PageProps) => {
   const [appeared, setAppeared] = useState(false)
 
   useEffect(() => {
     if (animated) timeout = setTimeout(() => setAppeared(true))
-    return () => clearTimeout(timeout)
+    return () => {
+      if (timeout) clearTimeout(timeout)
+    }
   }, [])
 
   return (
@@ -30,17 +32,17 @@ const Page = ({children, width, sx, animated = true, ...props}: PageProps) => {
         transform: 'scale(.90)',
         maxWidth: 932,
         width: '100%',
-        ...(!animated || appeared) && {
+        ...((!animated || appeared) && {
           opacity: 1,
           transform: 'scale(1)',
-        },
-        ...width && {maxWidth: width},
+        }),
+        ...(width && {maxWidth: width}),
         ...sx,
-      }}>
+      }}
+    >
       {children}
     </Box>
   )
 }
 
 export default Page
-
--- src/alexlibs/mui-extension/Txt/Txt.tsx
+++ src/alexlibs/mui-extension/Txt/Txt.tsx
@@ -11,99 +11,106 @@ interface Props extends BoxProps {
   size?: 'big' | 'title' | 'small'
   color?: 'primary' | 'secondary' | 'disabled' | 'hint' | 'default' | 'error'
   uppercase?: boolean
-  truncate?: boolean,
+  truncate?: boolean
   noWrap?: boolean
   link?: boolean
 }
 
-export const Txt = forwardRef(({
-  skeleton,
-  children,
-  gutterBottom,
-  block,
-  bold,
-  size,
-  link,
-  italic,
-  color,
-  uppercase,
-  truncate,
-  noWrap,
-  sx,
-  ...otherProps
-}: Props, ref: any) => {
-  return (
-    <Box
-      sx={{
-        display: 'inline',
-        lineHeight: '1.5',
-        ...size && ({
-          title: {
-            fontSize: '1.30rem',
-          },
-          big: {
-            fontSize: '1.10rem',
-          },
-          small: {
-            fontSize: '0.90rem',
-          },
-        })[size],
-        ...color && ({
-          primary: {
+export const Txt = forwardRef(
+  (
+    {
+      skeleton,
+      children,
+      gutterBottom,
+      block,
+      bold,
+      size,
+      link,
+      italic,
+      color,
+      uppercase,
+      truncate,
+      noWrap,
+      sx,
+      ...otherProps
+    }: Props,
+    ref: any,
+  ) => {
+    return (
+      <Box
+        sx={{
+          display: 'inline',
+          lineHeight: '1.5',
+          ...(size &&
+            {
+              title: {
+                fontSize: '1.30rem',
+              },
+              big: {
+                fontSize: '1.10rem',
+              },
+              small: {
+                fontSize: '0.90rem',
+              },
+            }[size]),
+          ...(color &&
+            {
+              primary: {
+                color: (t: any) => t.palette.primary.main,
+              },
+              secondary: {
+                color: (t: any) => t.palette.secondary.main,
+              },
+              disabled: {
+                color: (t: any) => t.palette.text.disabled,
+              },
+              hint: {
+                color: (t: any) => t.palette.text.secondary,
+              },
+              error: {
+                color: (t: any) => t.palette.error.main,
+              },
+              default: {
+                color: (t: any) => t.palette.text.primary,
+              },
+            }[color]),
+          ...(block && {
+            display: 'block',
+          }),
+          ...(bold && {
+            fontWeight: t => t.typography.fontWeightMedium,
+          }),
+          ...(italic && {
+            fontStyle: 'italic',
+          }),
+          ...(gutterBottom && {
+            mb: 1,
+          }),
+          ...(link && {
             color: t => t.palette.primary.main,
-          },
-          secondary: {
-            color: t => t.palette.secondary.main,
-          },
-          disabled: {
-            color: t => t.palette.text.disabled,
-          },
-          hint: {
-            color: t => t.palette.text.secondary,
-          },
-          error: {
-            color: t => t.palette.error.main,
-          },
-          default: {
-            color: t => t.palette.text.primary,
-          },
-        })[color],
-        ...block && {
-          display: 'block',
-        },
-        ...bold && {
-          fontWeight: t => t.typography.fontWeightMedium,
-        },
-        ...italic && {
-          fontStyle: 'italic',
-        },
-        ...gutterBottom && {
-          mb: 1,
-        },
-        ...link && {
-          color: t => t.palette.primary.main,
-        },
-        ...uppercase && {
-          textTransform: 'uppercase' as any,
-        },
-        ...noWrap && {
-          whiteSpace: 'nowrap' as any,
-        },
-        ...truncate && {
-          whiteSpace: 'nowrap' as any,
-          textOverflow: 'ellipsis',
-          overflow: 'hidden',
-        },
-        ...sx,
-      }}
-      {...otherProps}
-      ref={ref}
-    >
-      {skeleton ? (
-        <Skeleton sx={{display: 'inline-block'}} width={isNaN(skeleton as any) ? '80%' : skeleton as number}/>
-      ) : (
-        children
-      )}
-    </Box>
-  )
-})
+          }),
+          ...(uppercase && {
+            textTransform: 'uppercase' as any,
+          }),
+          ...(noWrap && {
+            whiteSpace: 'nowrap' as any,
+          }),
+          ...(truncate && {
+            whiteSpace: 'nowrap' as any,
+            textOverflow: 'ellipsis',
+            overflow: 'hidden',
+          }),
+          ...sx,
+        }}
+        {...otherProps}
+        ref={ref}
+      >
+        {skeleton ? (
+          <Skeleton sx={{display: 'inline-block'}} width={isNaN(skeleton as any) ? '80%' : (skeleton as number)} />
+        ) : (
+          children
+        )}
+      </Box>
+    )
+  },
+)
