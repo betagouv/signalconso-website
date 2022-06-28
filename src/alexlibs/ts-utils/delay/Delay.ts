db1f360477736e880576d22d885133990a271270
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2022-06-28 21:41:56 +0200

    remove unused sleep() from ts-utils

--- src/alexlibs/ts-utils/delay/Delay.ts
+++ src/alexlibs/ts-utils/delay/Delay.ts
@@ -2,5 +2,3 @@ export const delay =
   <T>(ms: number) =>
   (t: T): Promise<T> =>
     new Promise(resolve => setTimeout(() => resolve(t), ms))
-
-export const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
