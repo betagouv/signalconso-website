31d722b5e7522293e4fc74ea6ca6c1edb7c2b0b5
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2022-06-28 21:26:07 +0200

    remove unused globalprogressbar

--- src/alexlibs/mui-extension/GlobalProgress/index.ts
+++
@@ -1,3 +0,0 @@
-export {default as GlobalProgressProvider} from './GlobalProgressContext'
-export {withGlobalProgress, useGlobalProgress, useGlobalProgressState} from './GlobalProgressContext'
-export {default as GlobalProgressBar} from './GlobalProgressBar'
