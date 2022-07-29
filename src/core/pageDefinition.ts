1c1c877b239a288cabde5a903cc2a805b9081cb4
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2022-07-29 16:25:59 +0200

    delete unused pageDefinitions

--- src/core/pageDefinition.ts
+++ src/core/pageDefinition.ts
@@ -6,10 +6,6 @@ export interface PageDefinition {
 export type PageDefinitions = typeof pageDefinitions
 
 export const pageDefinitions = {
-  enterprisesImport: {
-    title: 'Base de données entreprise',
-    description: 'Gestion de la base de données entrerpises',
-  },
   default: {
     title: 'SignalConso, un service public pour les consommateurs',
     description:
@@ -39,10 +35,6 @@ export const pageDefinitions = {
     title: "Centre d'aide - SignalConso",
     description: "Consultez l'aide et les questions fréquentes sur SignalConso",
   },
-  retractation: {
-    title: 'Delai de rétractation ? - SignalConso',
-    description: 'Comprenez le délai de rétractation et calculez votre date limite de rétractation',
-  },
   accessibilite: {
     title: 'Accessibilité - SignalConso',
     description: "Rapport d'accessibilité SignalConso",
@@ -54,10 +46,6 @@ export const pageDefinitions = {
   conditionsGeneralesUtilisation: {
     title: "Conditions générales d'utilisation - SignalConso",
     description: "Consultez les conditions générales d'utilisation",
-  },
-  blog: {
-    title: 'Blog - SignalConso',
-    description: 'Les articles du blog de SignalConso',
   },
   stats: {
     title: 'Statistiques - SignalConso',
@@ -71,33 +59,9 @@ export const pageDefinitions = {
     title: 'Gestion des cookies - SignalConso',
     description: 'Consultez les informations concernant la gestion des cookies sur SignalConso',
   },
-  accesstoken: {
-    title: "Accès au compte de l'entreprise - SignalConso",
-    description: "Rejoindre le compte d'une entreprise",
-  },
   contact: {
     title: 'Contact - SignalConso',
     description: 'Trouvez le bon interlocuteur à contacter sur SignalConso',
-  },
-  account_login: {
-    title: 'Authentification - SignalConso',
-    description: 'Authentifiez-vous et accédez à votre espace personnel sur SignalConso',
-  },
-  account_emailValidation: {
-    title: 'Validation email - SignalConso',
-    description: 'Validation de votre email utilisateur sur SignalConso',
-  },
-  account_activation: {
-    title: "Activation de l'espace entreprise - SignalConso",
-    description: 'Activez votre espace entreprise sur SignalConso',
-  },
-  login_forgotPassword: {
-    title: 'Mot de passe oublié - SignalConso',
-    description: "Réinitialisez le mot de passe d'accès à votre espace personnel sur SignalConso",
-  },
-  login_resetPassword: {
-    title: 'Nouveau mot de passe - SignalConso',
-    description: "Réinitialisez le mot de passe d'accès à votre espace personnel sur SignalConso",
   },
   report_problem: {
     title: 'Le problème - SignalConso',
@@ -123,73 +87,9 @@ export const pageDefinitions = {
     title: 'Information - SignalConso',
     description: "Page d'information concernant votre signalement sur SignalConso",
   },
-  reports_detail: {
-    title: 'Détail du signalement - SignalConso',
-    description: 'Consultez le détail du signalement sur SignalConso',
-  },
-  reports_list: {
-    title: 'Suivi des signalements - SignalConso',
-    description: 'Consultez les signalements qui vous concernent sur SignalConso',
-  },
-  reports_review: {
-    title: 'Donnez votre avis - SignalConso',
-    description: "Donnez votre avis sur la réponse de l'entreprise à votre signalement sur SignalConso",
-  },
-  websites_manage: {
-    title: 'Modération des sites webs - SignalConso',
-    description: 'Modérer les sites webs suggérés',
-  },
-  websites_unregistered: {
-    title: 'Sites internet non identifiés - SignalConso',
-    description: 'Consultez la liste des sites internet signalés qui ne sont pas rattachés à une entreprise',
-  },
-  reportedPhones: {
-    title: 'Suivi des numéros de téléphones signalés - SignalConso',
-    description: 'Consultez la liste des numéros de téléphone signalés',
-  },
-  companies_myCompanies: {
-    title: 'Mes entreprises - SignalConso',
-    description: 'Liste des entreprises rattachées à votre espace personnel sur SignalConso',
-  },
-  companies_companyAccesses: {
-    title: 'Gestion des accès - SignalConso',
-    description: "Gestion des accès au compte de l'entreprise sur SignalConso",
-  },
-  companies_companyInvitation: {
-    title: "Accès au compte de l'entreprise - SignalConso",
-    description: "Ajout d'un accès au compte de l'entreprise",
-  },
-  companies_companiesAdmin: {
-    title: 'Entreprises - SignalConso',
-    description: 'Suivi des entreprises signalées sur SignalConso',
-  },
   contractualDispute: {
     title: "Résolution d'un problème individuel (litige) - SignalConso",
     description: 'Démarches conseillées par SignalConso pour résoudre un problème individuel (litige) avec une entreprise ',
-  },
-  secured_dgccrf: {
-    title: "Mode d'emploi DGCCRF - SignalConso",
-    description: "Mode d'emploi pour le profil back-office DGCCRF",
-  },
-  secured_account_changePassword: {
-    title: 'Changement de mot de passe - SignalConso',
-    description: "Changez votre mot de passe d'accès à votre espace personnel sur SignalConso",
-  },
-  secured_account_activation: {
-    title: 'Activation du compte - SignalConso',
-    description: 'Activez votre compte pour accéder à votre espace personnel sur SignalConso',
-  },
-  secured_subscriptions: {
-    title: 'Abonnements - SignalConso',
-    description: 'Abonnez-vous pour recevoir les signalements qui vous concernent',
-  },
-  secured_downloads: {
-    title: 'Téléchargements - SignalConso',
-    description: 'Mes téléchargements',
-  },
-  secured_admin: {
-    title: 'Admin - SignalConso',
-    description: 'Invitation à rejoindre SignalConso (DGCCRF)',
   },
   anomaly: {
     title: 'Arborescence',
