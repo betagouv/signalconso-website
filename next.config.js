ab63426aac0d969102fd0e597601d24b5d475504
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2022-08-05 16:13:51 +0200

    TRELLO-1393 transpile react-hook-form

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+
+// TRELLO-1393 the published files of react-hook-form seem not to be transpiled
+// to a low enough version of javascript
+// They use the object spread syntax { ... }, not supported by Safari 10
+const withTM = require('next-transpile-modules')(['react-hook-form'])
+
+module.exports = withTM({
   productionBrowserSourceMaps: true,
   reactStrictMode: true,
-}
+})
