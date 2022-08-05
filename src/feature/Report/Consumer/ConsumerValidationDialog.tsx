a8c9483d9f646841c837be27aba35af39f810dde
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2022-08-05 17:07:12 +0200

    attempt to simplify ConsumerValidationDialog

--- src/feature/Report/Consumer/ConsumerValidationDialog.tsx
+++ src/feature/Report/Consumer/ConsumerValidationDialog.tsx
@@ -1,13 +1,13 @@
-import {Button, Dialog, DialogActions, DialogContent, DialogTitle, Icon, LinearProgress} from '@mui/material'
-import React, {useEffect, useState} from 'react'
-import {Controller, useForm} from 'react-hook-form'
-import {useI18n} from 'core/i18n'
-import {ScButton} from 'shared/Button/Button'
-import {useFetcher} from '../../../alexlibs/react-hooks-lib'
-import {useApiSdk} from 'core/context/ApiSdk'
-import {Alert, Txt} from '../../../alexlibs/mui-extension'
-import {delay, duration, fnSwitch} from '../../../alexlibs/ts-utils'
 import {LoadingButton} from '@mui/lab'
+import {Button, Dialog, DialogActions, DialogContent, DialogTitle, Icon, LinearProgress} from '@mui/material'
+import {useApiSdk} from 'core/context/ApiSdk'
+import {useI18n} from 'core/i18n'
+import {useState} from 'react'
+import {Controller, useForm} from 'react-hook-form'
+import {ScButton} from 'shared/Button/Button'
+import {Alert, Txt} from '../../../alexlibs/mui-extension'
+import {useFetcher} from '../../../alexlibs/react-hooks-lib'
+import {delay, duration, fnSwitch} from '../../../alexlibs/ts-utils'
 import {InputValidationCode} from './InputValidationCode'
 
 interface Props {
@@ -30,16 +30,17 @@ export const ConsumerValidationDialog = ({loading, open, consumerEmail, onClose,
   const _checkEmail = useFetcher(apiSdk.consumerEmail.check)
   const [disableResendButton, setDisableResendButton] = useState(false)
 
-  const switchValidity = <T,>({valid, invalid, unknown}: {valid?: T; invalid?: T; unknown?: T}): T | undefined => {
-    const isEmailValid = _validateEmail.entity?.valid
-    if (isEmailValid) {
-      return valid
+  const isEmailValid: boolean | undefined = _validateEmail.entity?.valid
+
+  const onSubmitButtonClick = _form.handleSubmit(async form => {
+    if (!isEmailValid) {
+      const res = await _validateEmail.fetch({clean: false}, consumerEmail, form.code)
+      if (res.valid) {
+        await delay(500)
+        onValidated()
+      }
     }
-    if (isEmailValid === false) {
-      return invalid
-    }
-    return unknown
-  }
+  })
 
   return (
     <Dialog open={!!open} maxWidth="xs">
@@ -105,26 +106,16 @@ export const ConsumerValidationDialog = ({loading, open, consumerEmail, onClose,
         <Button color="primary" onClick={onClose}>
           {m.close}
         </Button>
-        {(() => {
-          const startIcon = switchValidity({valid: <Icon>check_circle</Icon>})
-          return (
-            <LoadingButton
-              color={switchValidity({valid: 'success', invalid: 'primary', unknown: 'primary'})}
-              onClick={_form.handleSubmit(form => {
-                if (_validateEmail.entity?.valid) return
-                _validateEmail.fetch({clean: false}, consumerEmail, form.code).then(res => {
-                  if (res.valid) delay(500)(res).then(onValidated)
-                })
-              })}
-              loading={_validateEmail.loading}
-              loadingPosition={startIcon ? 'start' : undefined}
-              startIcon={startIcon}
-              variant="contained"
-            >
-              {switchValidity({valid: m.validated, invalid: m.verify, unknown: m.verify})}
-            </LoadingButton>
-          )
-        })()}
+        <LoadingButton
+          color={isEmailValid ? 'success' : 'primary'}
+          onClick={onSubmitButtonClick}
+          loading={_validateEmail.loading}
+          loadingPosition="start"
+          startIcon={isEmailValid ? <Icon>check_circle</Icon> : undefined}
+          variant="contained"
+        >
+          {isEmailValid ? m.validated : m.verify}
+        </LoadingButton>
       </DialogActions>
     </Dialog>
   )
