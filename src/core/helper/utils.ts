dc45517a9340f8a784d547e8ae452c7992fd7160
ssedoudbgouv <s.sedoud.betagouv@gmail.com>
2022-08-08 13:05:28 +0200

    Revert "Disable local storage server side, to avoid annoying build log"
    This reverts commit c61438598159c5b2cace00d22719276f35a10c8e.

--- src/core/helper/utils.ts
+++ src/core/helper/utils.ts
@@ -11,5 +11,3 @@ export const classes = classNames
 const capitalize = (_: string) => {
   return _.charAt(0).toUpperCase() + _.slice(1)
 }
-
-export const isServerSide = () => typeof window === 'undefined'
