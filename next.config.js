306fa99c9c9115dcce0a84525f660642a751ad1d
ssedoudbgouv <s.sedoud.betagouv@gmail.com>
2022-08-08 13:03:54 +0200

    Revert "TRELLO-1393 transpile react-hook-form"
    This reverts commit 5b6d5e14ad81dba96e054cf02c67839abb67c2fd.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
-
-// TRELLO-1393 the published files of react-hook-form seem not to be transpiled
-// to a low enough version of javascript
-// They use the object spread syntax { ... }, not supported by Safari 10
-const withTM = require('next-transpile-modules')(['react-hook-form'])
-
-module.exports = withTM({
+module.exports = {
   productionBrowserSourceMaps: true,
   reactStrictMode: true,
-})
+}
