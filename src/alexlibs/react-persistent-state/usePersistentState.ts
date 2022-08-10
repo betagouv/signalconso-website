47f45e6ba975fc9fb0e407ffc697d1115a475163
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2022-08-10 15:53:45 +0200

    remove throttle of localstorage.save

--- src/alexlibs/react-persistent-state/usePersistentState.ts
+++ src/alexlibs/react-persistent-state/usePersistentState.ts
@@ -1,6 +1,5 @@
 import {LocalStorageEntity} from './localStorageApi'
 import {Dispatch, SetStateAction, useEffect, useMemo, useRef, useState} from 'react'
-import throttle from 'lodash.throttle'
 
 /**
  *
@@ -17,8 +16,7 @@ import throttle from 'lodash.throttle'
 export function usePersistentState<S>(initialState: S | (() => S), key: string): [S, Dispatch<SetStateAction<S>>, () => void] {
   const localStorage = useMemo(() => new LocalStorageEntity<S>(`react-persistant-state-${key}`), [])
   const [state, setState] = useState<S>(localStorage.load() ?? initialState)
-  const throttled = useRef(throttle(localStorage.save, 1000))
-  useEffect(() => throttled.current(state), [state])
+  useEffect(() => localStorage.save(state), [state])
 
   return [
     state,
