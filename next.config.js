fc96adfc017162e3f5fd3d19f281ebc46156d342
ssedoudbgouv <s.sedoud.betagouv@gmail.com>
2022-11-21 10:16:21 +0100

    TRELLO-1521: Add security headers

--- next.config.js
+++ next.config.js
@@ -15,7 +15,7 @@ const redirects = hostsToRedirect.map(host => ({
 
 const ContentSecurityPolicy = `
   default-src 'self';
-  connect-src https://api-adresse.data.gouv.fr ${process.env.NEXT_PUBLIC_APP_BASE_URL} ${process.env.NEXT_PUBLIC_API_BASE_URL} ${process.env.NEXT_PUBLIC_API_BASE_URL};
+  connect-src https://api-adresse.data.gouv.fr ${process.env.NEXT_PUBLIC_APP_BASE_URL} ${process.env.NEXT_PUBLIC_API_BASE_URL} ${process.env.NEXT_PUBLIC_COMPANY_API_BASE_URL};
   worker-src *.cleverapps.io *.conso.gouv.fr blob:;
   script-src-elem blob:;
   script-src 'self';
