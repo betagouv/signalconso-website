f3bb8286923f11b4ecc8ec2170bb28a85b0eebb3
ssedoudbgouv <85867707+ssedoudbgouv@users.noreply.github.com>
2022-11-24 09:01:41 +0100

    Trello 1521 (#187)
    * TRELLO-1521: Add security headers

--- next.config.js
+++ next.config.js
@@ -13,10 +13,63 @@ const redirects = hostsToRedirect.map(host => ({
   permanent: true,
 }))
 
+const ContentSecurityPolicy =
+  process.env.NEXT_PUBLIC_NODE_ENV === 'development'
+    ? ''
+    : `
+  default-src 'self';
+
+  connect-src 'self' https://api-adresse.data.gouv.fr ${process.env.NEXT_PUBLIC_API_BASE_URL} ${process.env.NEXT_PUBLIC_COMPANY_API_BASE_URL};
+  worker-src 'self' ${process.env.NEXT_PUBLIC_API_BASE_URL} ${process.env.NEXT_PUBLIC_COMPANY_API_BASE_URL} blob:;
+
+  img-src 'self' data: ${process.env.NEXT_PUBLIC_APP_BASE_URL} ${process.env.NEXT_PUBLIC_API_BASE_URL} *.cellar-c2.services.clever-cloud.com;
+  frame-src https://stats.data.gouv.fr/ https://www.youtube-nocookie.com;
+  frame-ancestors 'self';
+  child-src 'self';
+  style-src 'unsafe-inline';
+  font-src https://fonts.gstatic.com;
+  report-uri /csp-violation-report-endpoint/  
+`
+
+// script-src 'self';
+// script-src-elem 'self' blob: eval:;
+
+const securityHeaders = [
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+  },
+]
+
 module.exports = withTM({
   productionBrowserSourceMaps: true,
   reactStrictMode: true,
   async redirects() {
     return redirects
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
   },
 })
