9e94747d20186a6f797c1d6d902221c280a202aa
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2023-02-23 12:15:38 +0100

    remove useless ReportFlow component

--- src/components_simple/ReportFlowStepper/ReportFlowStepper.tsx
+++ src/components_simple/ReportFlowStepper/ReportFlowStepper.tsx
@@ -1,3 +1,4 @@
+import {useAnalyticContext} from 'analytic/AnalyticContext'
 import {Anomaly} from 'anomalies/Anomaly'
 import {Acknowledgement} from 'components_feature/Report/Acknowledgement/Acknowledgement'
 import {Company} from 'components_feature/Report/Company/Company'
@@ -5,15 +6,21 @@ import {Confirmation} from 'components_feature/Report/Confirmation/Confirmation'
 import {Consumer} from 'components_feature/Report/Consumer/Consumer'
 import {Details} from 'components_feature/Report/Details/Details'
 import {Problem} from 'components_feature/Report/Problem/Problem'
-import {getNextStep, getPreviousStep, ReportStep, ReportStepOrDone} from 'model/ReportStep'
+import {useReportFlowContext} from 'components_feature/Report/ReportFlowContext'
+import {
+  findCurrentStepForReport,
+  firstReportStep,
+  getAnalyticsForStep,
+  getNextStep,
+  getPreviousStep,
+  ReportStepOrDone,
+} from 'model/ReportStep'
 import {useEffect, useState} from 'react'
 import {scrollTop} from 'utils/utils'
 import {ReportFlowStepperHeader} from './ReportFlowStepperHeader'
 
 interface StepperProps {
-  initialStep: ReportStep
   anomaly: Anomaly
-  onStepChange: (step: ReportStepOrDone) => void
   isWebView: boolean
 }
 
@@ -24,14 +31,22 @@ export interface StepNavigation {
   prev: () => void
 }
 
-export const ReportFlowStepper = ({anomaly, initialStep, onStepChange, isWebView}: StepperProps) => {
+function useStepChangeTracking(anomaly: Anomaly, currentStep: ReportStepOrDone) {
+  const _analytics = useAnalyticContext()
+  useEffect(() => {
+    const {path, title} = getAnalyticsForStep(currentStep)
+    _analytics.trackPage(`/${anomaly.path}/${path}`, title)
+  }, [currentStep])
+}
+
+export const ReportFlowStepper = ({anomaly, isWebView}: StepperProps) => {
+  const _reportFlow = useReportFlowContext()
+  const initialStep =
+    anomaly.category === _reportFlow.reportDraft.category ? findCurrentStepForReport(_reportFlow.reportDraft) : firstReportStep
   const [currentStep, setCurrentStep] = useState<ReportStepOrDone>(initialStep)
+  useStepChangeTracking(anomaly, currentStep)
 
   const isDone = currentStep === 'Done'
-
-  useEffect(() => {
-    onStepChange(currentStep)
-  }, [currentStep])
 
   const goToNextStep = () => {
     if (isDone) return
