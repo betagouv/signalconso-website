cc8c20d7b90b5d99dec1edbe3407320b380b0947
Charles Dufour <charles.dufour@beta.gouv.fr>
2023-02-27 11:43:39 +0100

    Small refacto

--- src/components_simple/LinkBackToHome.tsx
+++ src/components_simple/LinkBackToHome.tsx
@@ -6,11 +6,7 @@ import {useI18n} from '../i18n/I18n'
 export const LinkBackToHome = ({isWebView}: {isWebView: boolean}) => {
   const {m} = useI18n()
 
-  const sendMessageToReactNative = () => {
-    if (window.ReactNativeWebView) {
-      window.ReactNativeWebView.postMessage('success')
-    }
-  }
+  const sendMessageToReactNative = () => window.ReactNativeWebView?.postMessage('success')
 
   return isWebView ? (
     <ScButton onClick={sendMessageToReactNative} color="primary" variant="contained" icon="home">
