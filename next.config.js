3750c2649b0fa677591ce7aa8d2ad908663fccbb
Charles Dufour <charles.dufour@beta.gouv.fr>
2023-03-09 09:14:54 +0100

    Fix unsafe-eval

--- next.config.js
+++ next.config.js
@@ -17,7 +17,7 @@ const redirects = hostsToRedirect.map(host => ({
 // See https://nextjs.org/docs/advanced-features/security-headers for implementation details
 
 const CommonContentSecurityPolicy = [
-  `default-src 'self' *.aticdn.net *.data.gouv.fr;`,
+  `default-src 'self' 'unsafe-eval' *.aticdn.net *.data.gouv.fr;`,
   `connect-src 'self' *.sentry.io *.data.gouv.fr ${process.env.NEXT_PUBLIC_API_BASE_URL} ${process.env.NEXT_PUBLIC_COMPANY_API_BASE_URL};`,
   `worker-src \'self\' ${process.env.NEXT_PUBLIC_API_BASE_URL} ${process.env.NEXT_PUBLIC_COMPANY_API_BASE_URL} blob:;`,
   `img-src 'self' data: ${process.env.NEXT_PUBLIC_APP_BASE_URL} ${process.env.NEXT_PUBLIC_API_BASE_URL} *.cellar-c2.services.clever-cloud.com *.xiti.com;`,
