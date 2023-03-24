0e9da0dc77871550e65007f4156f62c36ce1d6c2
ssedoudbgouv <s.sedoud.betagouv@gmail.com>
2023-03-24 09:14:06 +0100

    TRELLO-1547: fix CSP rules

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -74,6 +74,7 @@ const _App = ({Component, pageProps, router}: AppProps) => {
       </Head>
       {!isWebView && (
         <Script
+          nonce="eYhD6rb8vLVwXsAmnbKl/Q=="
           id="eulerian-analytics"
           dangerouslySetInnerHTML={{
             __html: `(function(e,a){var i=e.length,y=5381,k='script',s=window,v=document,o=v.createElement(k);for(;i;){i-=1;y=(y*33)^e.charCodeAt(i)}y='_EA_'+(y>>>=0);(function(e,a,s,y){s[a]=s[a]||function(){(s[y]=s[y]||[]).push(arguments);s[y].eah=e;};}(e,a,s,y));i=new Date/1E7|0;o.ea=y;y=i%26;o.async=1;o.src='//'+e+'/'+String.fromCharCode(97+y,122-y,65+y)+(i%1E3)+'.js?2';s=v.getElementsByTagName(k)[0];s.parentNode.insertBefore(o,s);})('wykp.signal.conso.gouv.fr','EA_push');`,
