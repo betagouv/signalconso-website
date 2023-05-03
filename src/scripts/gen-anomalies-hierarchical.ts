3fdaea64f065103657ada35f7c9f32ca849178b8
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2023-05-03 13:20:14 +0200

    use toml

--- src/scripts/gen-anomalies-hierarchical.ts
+++ src/scripts/gen-anomalies-hierarchical.ts
@@ -4,7 +4,7 @@ import fs from 'fs'
 import {rimrafSync} from 'rimraf'
 import {Subcategory} from 'anomalies/Anomaly'
 import slugify from 'slugify'
-
+import toml from '@iarna/toml'
 const targetDir = path.resolve('./src/anomalies/hierarchical')
 
 function start() {
@@ -23,16 +23,16 @@ function recurse(parentDir: string, subcat: Subcategory, idxInParent: number) {
   if (hasChildren) {
     const folderPath = path.join(parentDir, `${nameInFileSystem}`)
     createDir(folderPath)
-    const filePath = path.join(folderPath, '__index.json')
+    const filePath = path.join(folderPath, '__index.toml')
     const {subcategories, ...rest} = subcat
-    createJsonFile(filePath, rest)
+    createTomlFile(filePath, rest)
 
     subcategories?.forEach((subsubcat, idx) => {
       recurse(folderPath, subsubcat, idx)
     })
   } else {
-    const filePath = path.join(parentDir, `${nameInFileSystem}.json`)
-    createJsonFile(filePath, subcat)
+    const filePath = path.join(parentDir, `${nameInFileSystem}.toml`)
+    createTomlFile(filePath, subcat)
   }
 }
 
@@ -54,6 +54,12 @@ function createJsonFile(path: string, content: unknown): void {
   console.log('Creating ', path)
   const data = JSON.stringify(content, null, 2)
   fs.writeFileSync(path, data, 'utf8')
+}
+
+function createTomlFile(path: string, content: unknown): void {
+  console.log('Creating ', path)
+  const tomlContent = toml.stringify(content as any)
+  fs.writeFileSync(path, tomlContent, 'utf8')
 }
 
 function padTo2(num: number): string {
