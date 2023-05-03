cb5a8fa00687ea47194ebd61451ee886748b403f
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2023-05-03 15:53:37 +0200

    suite

--- src/scripts/gen-anomalies-hierarchical.ts
+++ src/scripts/gen-anomalies-hierarchical.ts
@@ -1,38 +1,104 @@
-import {allAnomalies} from '../anomalies/Anomalies'
-import path from 'path'
-import fs from 'fs'
-import {rimrafSync} from 'rimraf'
-import {Subcategory} from 'anomalies/Anomaly'
-import slugify from 'slugify'
 import toml from '@iarna/toml'
+import {Anomaly, Subcategory} from 'anomalies/Anomaly'
+import fs from 'fs'
+import fsExtra from 'fs-extra'
+import yaml from 'js-yaml'
+import path from 'path'
+import {rimrafSync} from 'rimraf'
+import slugify from 'slugify'
+import {allAnomalies, instanceOfAnomaly} from '../anomalies/Anomalies'
+import * as yamlImport from 'yaml-import'
+
+const classicYmlRoot = path.resolve('./src/anomalies/yml')
+const tmpYmlRoot = path.resolve('./src/anomalies/yml2')
 const targetDir = path.resolve('./src/anomalies/hierarchical')
+const FILE_FORMAT: 'json' | 'toml' | 'yaml' = 'yaml'
 
 function start() {
-  console.log('targetDir', targetDir)
+  startFromCurrentYaml()
+}
+
+function startGenFromJson() {
   resetDir(targetDir)
   allAnomalies.forEach((anomaly, idx) => {
     recurse(targetDir, anomaly, idx)
   })
 }
 
+function startFromCurrentYaml() {
+  resetDir(targetDir)
+  removeWholeDir(tmpYmlRoot)
+  copyWholeDir(classicYmlRoot, tmpYmlRoot)
+  rewriteImports()
+
+  const tmpYamlFile = path.resolve(tmpYmlRoot, 'tmp.yml')
+  const ymlFileRoot = path.join(tmpYmlRoot, 'anomalies.yml')
+  console.log(`Reading ${ymlFileRoot}, resolving imports and writing into ${tmpYamlFile}`)
+  yamlImport.write(ymlFileRoot, tmpYamlFile)
+  // Technically the types are inexact because of the imports, but it should correct enough for this use case
+  const anomaliesWithCustomImports = yaml.load(readFileRaw(tmpYamlFile)) as Anomaly[]
+  removeFile(tmpYamlFile)
+  anomaliesWithCustomImports.forEach((anomaly, idx) => {
+    recurse(targetDir, anomaly, idx)
+  })
+}
+
+// Rewrite all the :
+//
+// foo: !!import/single xxxx
+//
+// as
+//
+// foo:
+//   customimport: xxx
+//
+// but preserve the ones in anomalies.yml
+function rewriteImports() {
+  forEachFileInDirectory(tmpYmlRoot, yamlPath => {
+    if (yamlPath.endsWith('anomalies.yml')) {
+      console.log('Preserving !!import statements in ', yamlPath)
+    } else {
+      let fileContents = readFileRaw(yamlPath)
+      if (fileContents.includes('!!import/single')) {
+        console.log('Rewriting imports found in', yamlPath)
+        const importStatementRegex = /^(\s*)(\w+): !!import\/single (\S+)/gm
+        let match = importStatementRegex.exec(fileContents)
+        while (match !== null) {
+          const fullMatch = match[0]
+          const indent = match[1]
+          const key = match[2]
+          const importPath = match[3]
+          const replacement = `${indent}${key}:
+${indent}  customimport: ${importPath}`
+          fileContents = fileContents.replace(fullMatch, replacement)
+          match = importStatementRegex.exec(fileContents)
+        }
+        fs.writeFileSync(yamlPath, fileContents, 'utf-8')
+      }
+    }
+  })
+}
+
 function recurse(parentDir: string, subcat: Subcategory, idxInParent: number) {
-  const hasChildren = subcat.subcategories && subcat.subcategories.length > 0
-
   const nameInFileSystem = `${padTo2(idxInParent + 1)}_${slugify(subcat.title, '_')}`
+  const {subcategories, id, ...rest} = subcat
+  const fileContent = {
+    // preserve id only at the top level
+    ...(instanceOfAnomaly(subcat) ? {id} : {}),
+    ...rest,
+  }
 
-  if (hasChildren) {
+  if (subcategories && subcategories.length > 0) {
     const folderPath = path.join(parentDir, `${nameInFileSystem}`)
     createDir(folderPath)
-    const filePath = path.join(folderPath, '__index.toml')
-    const {subcategories, ...rest} = subcat
-    createTomlFile(filePath, rest)
-
+    const filePath = path.join(folderPath, `__index.${FILE_FORMAT}`)
+    createFile(filePath, fileContent, FILE_FORMAT)
     subcategories?.forEach((subsubcat, idx) => {
       recurse(folderPath, subsubcat, idx)
     })
   } else {
-    const filePath = path.join(parentDir, `${nameInFileSystem}.toml`)
-    createTomlFile(filePath, subcat)
+    const filePath = path.join(parentDir, `${nameInFileSystem}.${FILE_FORMAT}`)
+    createFile(filePath, fileContent, FILE_FORMAT)
   }
 }
 
@@ -40,30 +106,62 @@ function resetDir(path: string): void {
   console.log('Resetting directory', path)
   const exists = fs.existsSync(path)
   if (exists) {
-    console.log('Rimraf', path)
     rimrafSync(path)
   }
   fs.mkdirSync(path, {recursive: true})
+}
+
+function removeWholeDir(path: string): void {
+  console.log('Removing if it exists', path)
+  const exists = fs.existsSync(path)
+  if (exists) {
+    rimrafSync(path)
+  }
+}
+
+function removeFile(filePath: string): void {
+  fs.unlinkSync(filePath)
 }
 
 function createDir(path: string): void {
   fs.mkdirSync(path, {recursive: true})
 }
 
-function createJsonFile(path: string, content: unknown): void {
+function createFile(path: string, content: unknown, format: typeof FILE_FORMAT): void {
   console.log('Creating ', path)
-  const data = JSON.stringify(content, null, 2)
-  fs.writeFileSync(path, data, 'utf8')
-}
-
-function createTomlFile(path: string, content: unknown): void {
-  console.log('Creating ', path)
-  const tomlContent = toml.stringify(content as any)
-  fs.writeFileSync(path, tomlContent, 'utf8')
+  let contentStr =
+    format === 'json'
+      ? JSON.stringify(content, null, 2)
+      : format === 'toml'
+      ? toml.stringify(content as any)
+      : yaml.dump(content as any)
+  fs.writeFileSync(path, contentStr, 'utf8')
 }
 
 function padTo2(num: number): string {
   return num.toString().padStart(2, '0')
+}
+
+function copyWholeDir(source: string, destination: string) {
+  console.log(`Copying dir ${source} to ${destination}`)
+  fsExtra.copySync(source, destination)
+}
+
+function readFileRaw(filePath: string): string {
+  return fs.readFileSync(filePath, 'utf-8')
+}
+
+function forEachFileInDirectory(directory: string, callback: (filePath: string) => void) {
+  const files = fs.readdirSync(directory)
+  for (const file of files) {
+    const filePath = path.join(directory, file)
+    const stats = fs.statSync(filePath)
+    if (stats.isFile()) {
+      callback(filePath)
+    } else if (stats.isDirectory()) {
+      forEachFileInDirectory(filePath, callback)
+    }
+  }
 }
 
 start()
