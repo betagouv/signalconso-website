5de817ce6c62754c15ea0d361a97fd4074fb056e
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2023-05-03 11:19:05 +0200

    suite

--- src/anomalies/script/gen-tmp.ts
+++ src/anomalies/script/gen-tmp.ts
@@ -1,7 +1,7 @@
 import {allAnomalies} from '../Anomalies'
 import path from 'path'
 import fs from 'fs'
-import {rimraf} from 'rimraf'
+import {rimrafSync} from 'rimraf'
 import {Subcategory} from 'anomalies/Anomaly'
 import slugify from 'slugify'
 
@@ -11,16 +11,16 @@ function start() {
   console.log('targetDir', targetDir)
 
   resetDir(targetDir)
-  allAnomalies.forEach(anomaly => {
-    recurse(targetDir, anomaly)
+  allAnomalies.forEach((anomaly, idx) => {
+    recurse(targetDir, anomaly, idx)
   })
 }
 
-function recurse(parentDir: string, subcat: Subcategory) {
+function recurse(parentDir: string, subcat: Subcategory, idxInParent: number) {
   const hasChildren = subcat.subcategories && subcat.subcategories.length > 0
 
   // il faudra vérifier que les slugs sont uniques...
-  const nameInFileSystem = slugify(subcat.title, '_')
+  const nameInFileSystem = `${padTo2(idxInParent + 1)}_${slugify(subcat.title, '_')}`
 
   if (hasChildren) {
     const folderPath = path.join(parentDir, `${nameInFileSystem}`)
@@ -29,8 +29,8 @@ function recurse(parentDir: string, subcat: Subcategory) {
     const {subcategories, ...rest} = subcat
     createJsonFile(filePath, rest)
 
-    subcategories?.forEach(subsubcat => {
-      recurse(folderPath, subsubcat)
+    subcategories?.forEach((subsubcat, idx) => {
+      recurse(folderPath, subsubcat, idx)
     })
   } else {
     const filePath = path.join(parentDir, `${nameInFileSystem}.json`)
@@ -42,7 +42,8 @@ function resetDir(path: string): void {
   console.log('Resetting directory', path)
   const exists = fs.existsSync(path)
   if (exists) {
-    rimraf.sync(`${path}/*`)
+    console.log('Rimraf', path)
+    rimrafSync(path)
   }
   fs.mkdirSync(path, {recursive: true})
 }
@@ -55,6 +56,10 @@ function createJsonFile(path: string, content: unknown): void {
   console.log('Creating ', path)
   const data = JSON.stringify(content, null, 2)
   fs.writeFileSync(path, data, 'utf8')
+}
+
+function padTo2(num: number): string {
+  return num.toString().padStart(2, '0')
 }
 
 start()
