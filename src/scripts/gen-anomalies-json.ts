6aba92fc508988ccb1ba3b06b4c31091f416e2d4
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2023-05-16 12:56:49 +0200

    TRELLO-1747 New files/folders-based YML structure

---
+++ src/scripts/gen-anomalies-json.ts
@@ -0,0 +1,131 @@
+import fs from 'fs'
+import mapValues from 'lodash/mapValues'
+import path from 'path'
+import {checkAnomaliesYaml} from '../anomalies/checks/checkAnomaliesJson'
+import {createDirIfNeeded, dirContentSorted, exists, isFile, readFileYaml, sortObjectKeys, throwIfNotExists} from './scriptUtils'
+
+const rootDir = path.resolve('./src/anomalies/yml')
+const jsonOutputDir = path.resolve('./src/anomalies/json')
+const jsonOutputFile = path.resolve(jsonOutputDir, 'anomalies.json')
+const INDEX_YAML = '_index.yaml'
+
+// Attempt to read the new file tree structure from 'yml' folder
+// and resolve imports
+function readAnomaliesFromYmlFileTree() {
+  console.log('Reading everything in', rootDir)
+  const anomalyDirs = dirContentSorted(rootDir, {excludedFileNames: ['_imports']})
+  const anomalies = anomalyDirs.map(_ => {
+    return resolveImportsRecursively(buildSubcategoryFromFileOrFolder(_))
+  })
+  checkAnomaliesYaml(anomalies)
+  console.log(`The YAML is valid`)
+  addUniqueId(anomalies)
+  createDirIfNeeded(jsonOutputDir)
+  console.log(`Generating JSON file ${jsonOutputFile}`)
+  fs.writeFileSync(jsonOutputFile, JSON.stringify(sortObjectKeys(anomalies), null, 2))
+}
+
+// Read a file or a folder
+// Returns a Subcategory
+// except it can use 'customimport', so it's not exactly the correct type
+function buildSubcategoryFromFileOrFolder(_path: string): any {
+  // console.log('Reading ', _path)
+  throwIfNotExists(_path)
+  if (isFile(_path)) {
+    return readFileYaml(_path)
+  }
+  // It's a directory
+  // Read _index.yaml
+  const indexFile = path.join(_path, INDEX_YAML)
+  throwIfNotExists(indexFile, `Missing file ${INDEX_YAML} in ${_path}`)
+  if (!isFile(indexFile)) {
+    throw new Error(`${indexFile} is supposed to be a file, not a directory`)
+  }
+  const indexYaml: any = readFileYaml(indexFile)
+  // Read the rest (each file or folder turns into a subcat of this subcat)
+  const otherFilesOrSubdirs = dirContentSorted(_path, {excludedFileNames: [INDEX_YAML]})
+  const subSubcats = otherFilesOrSubdirs.map(subpath => {
+    return buildSubcategoryFromFileOrFolder(subpath)
+  })
+  // Then merge
+  const subcat = {
+    ...indexYaml,
+    subcategories: subSubcats,
+  }
+  return subcat
+}
+
+// Read a file or folder (within _imports)
+// Build the object or array that we're supposed to import
+//
+// The logic is slightly different from the main method, if it's a folder :
+//  - we don't expect an _index.yaml
+//  - we build an array of subcategories for each file/subdir
+//    (instead of building the subcategory above)
+function buildImportableFromFileOrFolder(_path: string): any {
+  // console.log('Reading ', _path)
+  if (!exists(_path)) {
+    throw new Error(`Nothing at path ${_path}`)
+  }
+  if (isFile(_path)) {
+    return readFileYaml(_path)
+  } else {
+    // Read the rest (each file or folder turns into a subcat of this subcat)
+    const filesOrSubdirs = dirContentSorted(_path)
+    const subcats = filesOrSubdirs.map(subpath => {
+      // recurse with the other method
+      return buildSubcategoryFromFileOrFolder(subpath)
+    })
+    return subcats
+  }
+}
+
+function resolveImportsRecursively(obj: any): any {
+  if (Array.isArray(obj)) {
+    return obj.map(resolveImportsRecursively)
+  }
+  if (typeof obj === 'object' && obj !== null) {
+    // It's an object
+    // Resolve imports
+    const {customimport, ...rest} = obj
+    let imported = {}
+    if (customimport) {
+      checkCustomImportIsValid(customimport)
+      // console.log('Resolving import of ', customimport)
+      const fullImportPath = path.join(rootDir, customimport)
+      imported = buildImportableFromFileOrFolder(fullImportPath)
+    }
+    if (Array.isArray(imported)) {
+      return imported.map(resolveImportsRecursively)
+    } else {
+      const obj2 = Array.isArray(imported) ? imported : {...rest, ...imported}
+      // Then recurse
+      const obj3 = mapValues(obj2, v => {
+        return resolveImportsRecursively(v)
+      })
+      return obj3
+    }
+  }
+  return obj
+}
+
+function checkCustomImportIsValid(customimport: string) {
+  // make sure that there is not funny import path
+  const validPaths = ['_imports/subcategories/', '_imports/blockingInfo/', '_imports/detailInputs/']
+  if (!validPaths.some(_ => customimport.startsWith(_))) {
+    throw new Error(`Import of "${customimport}" is invalid, it should start with one of these ${validPaths.join(', ')}`)
+  }
+}
+
+function addUniqueId(obj: any, depth = 0, prefix?: string): void {
+  let index = 1
+  obj.forEach((entry: any) => {
+    const id = `${prefix ? prefix + '.' : ''}${entry.id || index++}`
+    entry.id = id
+    if (entry.subcategories) {
+      addUniqueId(entry.subcategories, depth + 1, id)
+    }
+  })
+}
+
+readAnomaliesFromYmlFileTree()
---
+++ src/scripts/scriptUtils.ts
@@ -0,0 +1,74 @@
+import fs from 'fs'
+import yaml from 'js-yaml'
+import sortBy from 'lodash/sortBy'
+import path from 'path'
+
+// Sort an object keys, recursively
+// useful to compare JSONs
+export function sortObjectKeys(obj: any): any {
+  if (Array.isArray(obj)) {
+    return obj.map(sortObjectKeys)
+  }
+  if (typeof obj === 'object' && obj !== null) {
+    const keys = Object.keys(obj).sort()
+    const res: any = {}
+    keys.forEach(k => {
+      const value = obj[k]
+      res[k] = sortObjectKeys(value)
+    })
+    return res
+  }
+  return obj
+}
+
+export function readFileYaml(filePath: string): any {
+  return yaml.load(fs.readFileSync(filePath, 'utf-8'))
+}
+
+export function exists(_path: string) {
+  return fs.existsSync(_path)
+}
+export function isFile(_path: string) {
+  return exists(_path) && fs.statSync(_path).isFile()
+}
+export function throwIfNotExists(_path: string, message?: string) {
+  if (!exists(_path)) {
+    throw new Error(message ?? `Nothing at path ${_path}`)
+  }
+}
+export function throwIfNotDir(_path: string, message?: string) {
+  if (isFile(_path)) {
+    throw new Error(message ?? `This is a file, not a directory: ${_path}`)
+  }
+}
+export function throwIfNotFile(_path: string, message?: string) {
+  if (!isFile(_path)) {
+    throw new Error(message ?? `This is a file, not a directory: ${_path}`)
+  }
+}
+export function createDirIfNeeded(_path: string): void {
+  if (!exists(_path)) {
+    console.log(`Creating directory ${_path}`)
+    fs.mkdirSync(_path, {recursive: true})
+  }
+}
+
+export function renameFileOrDir(_path: string, newFilename: string): void {
+  const newPath = path.join(path.dirname(_path), newFilename)
+  if (newPath !== _path) {
+    console.log(`Renaming ${_path} -> ${newFilename}`)
+    fs.renameSync(_path, newPath)
+  }
+}
+
+// List both files and subfolders
+// Sort them by name
+export function dirContentSorted<A>(dirPath: string, {excludedFileNames}: {excludedFileNames?: string[]} = {}) {
+  return sortBy(fs.readdirSync(dirPath), _ => _)
+    .filter(_ => !excludedFileNames?.includes(_))
+    .map(_ => path.join(dirPath, _))
+}
+
+export function padTo2(num: number): string {
+  return num.toString().padStart(2, '0')
+}
