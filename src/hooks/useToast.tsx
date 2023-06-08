32ab589740a8cde1fc9e8724cf19806ea7460aec
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2023-06-08 09:31:56 +0200

    TRELLO-1433 rework errors Toast, handle empty file better (#356)

--- src/hooks/useToast.tsx
+++ src/hooks/useToast.tsx
@@ -1,24 +1,37 @@
-import {useToast as useMuiToast} from '../alexlibs/mui-extension/Toast/Toast'
+import {useToastContext} from '../alexlibs/mui-extension/Toast/ToastContext'
 import {useI18n} from '../i18n/I18n'
 import {ApiError} from '../clients/BaseApiClient'
 
 export const useToast = () => {
-  const {toastError, ...toasts} = useMuiToast()
+  const {toastError, ...toasts} = useToastContext()
   const {m} = useI18n()
 
-  const getErrorMessage = (err: Partial<ApiError>) => {
-    console.error(JSON.stringify(err.details))
-    if (err.details?.id && (m.apiErrorsCode as any)[err.details.id]) {
-      return (m.apiErrorsCode as any)[err.details.id]
+  function getErrorMessage(err: unknown): string {
+    console.error('Got error', err)
+    if (typeof err === 'string') {
+      return err
     }
-    if (err.message && err.message !== '') {
+    if (isApiError(err)) {
+      if (err.details.id && (m.apiErrorsCode as any)[err.details.id]) {
+        return (m.apiErrorsCode as any)[err.details.id]
+      }
+    }
+    if (isError(err)) {
       return err.message
     }
     return m.anErrorOccurred
   }
 
   return {
-    toastError: (error: Partial<ApiError>) => toastError(getErrorMessage(error)),
+    toastError: (error: unknown) => toastError(getErrorMessage(error)),
     ...toasts,
   }
+}
+
+function isApiError(err: unknown): err is ApiError {
+  return (err as any).name === 'ApiError'
+}
+
+function isError(err: unknown): err is Error {
+  return err instanceof Error
 }
