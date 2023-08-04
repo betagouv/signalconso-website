768e1f7ae1b1be28633ccf71d3443c46663507c1
Charles Dufour <charles.dufour@beta.gouv.fr>
2023-08-04 16:37:00 +0200

    [TRELLO-1931] Publish accessibility article (#427)

--- src/actualites/newsArticlesData.ts
+++ src/actualites/newsArticlesData.ts
@@ -1,5 +1,6 @@
 import {ArticleAppMobile} from './ArticleAppMobile'
 import {ArticleQuestionnaireSiteDgccrf} from './ArticleQuestionnaireSiteDgccrf'
+import {ArticleAccessibilite} from './ArticleAccessibilite'
 
 export type NewsArticle = (typeof newsArticlesData)[number]
 
@@ -21,5 +22,14 @@ export const newsArticlesData = [
     excerpt: `Découvrez dès maintenant la nouvelle application mobile SignalConso, qui rend vos signalements en tant que consommateur
     encore plus faciles et simplifie vos démarches.`,
     content: ArticleAppMobile,
+  },
+  {
+    date: '2023-08-04',
+    slug: 'signalconso-une-accessibilite-renforcee-pour-tous-les-consommateurs',
+    title: 'SignalConso : Une Accessibilité Renforcée pour tous les Consommateurs',
+    // used for SEO description
+    // used for the preview in /news
+    excerpt: `SignalConso : Une Accessibilité Renforcée pour tous les Consommateurs`,
+    content: ArticleAccessibilite,
   },
 ]
