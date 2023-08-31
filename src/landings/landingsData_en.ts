71f5799e51283c4d0fe6663e2d332e06e7d74329
ssedoudbgouv <s.sedoud.betagouv@gmail.com>
2023-08-31 16:04:22 +0200

    TRELLO-1828 : english landing

--- src/landings/landingsData_en.ts
+++ src/landings/landingsData_en.ts
@@ -1,4 +1,20 @@
 // ----------------------------------------------
   // ---- Generated file, do not edit manually ---
   // ----------------------------------------------
-  export const enLandingsData = [];
+  export const enLandingsData = [
+  {
+    "url": "test-en",
+    "lang": "en",
+    "isSemiAutomatic": false,
+    "seoTitle": "Test en",
+    "seoDescription": "SEO desc test EN",
+    "title": "Title test en",
+    "targetedCategory": [
+      "AchatMagasin"
+    ],
+    "catchPhrase": "accroche test en accroche test en accroche test en accroche test en accroche test en accroche test en accroche test en accroche test en accroche test en accroche test en accroche test en",
+    "secondaryTitle1": "Permier sous titre EN",
+    "secondaryTitle2": "deuxieme sous titre en ",
+    "sampleReports": []
+  }
+];
