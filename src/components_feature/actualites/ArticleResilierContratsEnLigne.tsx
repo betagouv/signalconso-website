76dc62a157a698fd0284fcd240d562092226af57
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2023-09-14 12:31:02 +0200

    added some internal links to landings

--- src/components_feature/actualites/ArticleResilierContratsEnLigne.tsx
+++ src/components_feature/actualites/ArticleResilierContratsEnLigne.tsx
@@ -1,60 +1,75 @@
 'use client'
 
+import {buildHardcodedLinkLandingPageFr} from '../../core/pagesDefinitions'
 import Image from 'next/image'
+import Link from 'next/link'
+import {ReactNode} from 'react'
 import {useI18n} from '../../i18n/I18n'
 import {AppLangs} from '../../i18n/localization/AppLangs'
-import Link from 'next/link'
+
+function LandingLink({path, children}: {path: string; children: ReactNode}) {
+  const fullUrl = buildHardcodedLinkLandingPageFr(path)
+  if (!fullUrl) {
+    // it's not worth throwing an error
+    console.warn(`Didn't find landing ${path}`)
+    return children
+  }
+  return <Link href={fullUrl}>{children}</Link>
+}
 
 export function ArticleResilierContratsEnLigne() {
   const {m, currentLang} = useI18n()
   return (
     <div>
       <p>
-        Fini les lettres recommandées avec accusé de réception pour mettre fin à son contrat d'assurance, d'abonnement à une
-        plateforme de streaming, à un opérateur téléphonique ou à une salle de sport !!
+        Oubliez les lettres recommandées avec accusé de réception pour mettre fin à votre{' '}
+        <LandingLink path={'banque-assurance-mutuelle'}>contrat d'assurance</LandingLink>, d'abonnement à une{' '}
+        <LandingLink path={'tel-internet-media'}>plateforme de streaming</LandingLink>, à un{' '}
+        <LandingLink path={'tel-internet-media'}>opérateur téléphonique</LandingLink> ou à une{' '}
+        <LandingLink path={'voyage-loisirs'}>salle de sport</LandingLink>, ou encore{' '}
+        <LandingLink path={'eau-gaz-electricite'}>d'énergie</LandingLink> !
       </p>
       <p>
         <span className="font-bold">
-          Les contrats conclus ou pouvant être conclus en ligne, peuvent être résiliés en quelques clics, c'est à dire rapidement
-          et en ligne
+          <LandingLink path={'achat-site'}>Tous ces contrats</LandingLink> que vous avez conclus ou que vous pourriez conclure en
+          ligne peuvent être résiliés en deux temps trois mouvements, c'est-à-dire rapidement et en ligne
         </span>
-        . Cette nouvelle disposition, prévue par la loi du 16 août 2022 portant mesure d'urgence pour la protection du pouvoir
-        d'achat, facilite les démarches du consommateur qui souhaite choisir une offre plus intéressante ou renoncer à un service.
-        Elle ne change pas les conditions de résiliation du contrat mais offre une nouvelle modalité de résiliation au
-        consommateur.
+        . Grâce à cette nouvelle disposition, qui a été mise en place par la loi du 16 août 2022 pour protéger votre pouvoir
+        d'achat, vous allez pouvoir vous simplifier la vie si vous voulez changer pour une offre plus intéressante ou tout
+        simplement renoncer à un service. Elle ne change pas les conditions de résiliation de votre contrat, mais elle vous donne
+        une nouvelle option pour résilier.
       </p>
       <p>
-        Comment ça marche ? Les sites Internet et applications mobiles devront comporter un bouton de résiliation facilement et
-        directement accessible par leurs clients souhaitant résilier leur contrat.
+        Mais comment ça marche concrètement ? Les sites web et les applis mobiles doivent maintenant présenter bien en évidence un
+        bouton de résiliation pour que vous puissiez résilier votre contrat facilement.
       </p>
-      <p>
-        Le consommateur n'aura plus qu'à :
-        <ul>
-          <li>
-            Cliquer sur ce bouton de résiliation sur le site Internet ou l'application mobile. Le professionnel pourra rappeler
-            les informations sur les conditions de la résiliation du contrat (respect d'un délai de préavis, paiement d'une
-            indemnité de résiliation ...)
-          </li>
-          <li>
-            Indiquer son nom, ses coordonnées électroniques ou postales, la référence du contrat ou confirmer ces informations
-            lorsqu'elles sont pré-enregistrées par le professionnel
-          </li>
-          <li>
-            Pour les cas où les contrats sont résiliés de façon anticipée, indiquer le cas échéant le motif de résiliation et
-            envoyer les pièces justificatives (par exemple les contrats d'accès à Internet peuvent être résiliées sans frais en
-            cas de surendettement)
-          </li>
-          <li>Il sera alors redirigé vers une page récapitulative lui permettant de vérifier les informations fournies</li>
-          <li>
-            Il pourra alors valider pour finaliser sa démarche et notifier sa résiliation au professionnel en cliquant sur un
-            bouton « notification de la résiliation »
-          </li>
-          <li>
-            L'entreprise doit confirmer la réception de la décision de résiliation, puis informer le consommateur, dans un délai
-            raisonnable, de la date et des effets de la résiliation.
-          </li>
-        </ul>
-      </p>
+      <p>Le consommateur n'aura plus qu'à :</p>
+      <ul>
+        <li>
+          Vous cliquez sur le bouton de résiliation sur le site web ou l'appli mobile. Le professionnel vous rappellera les
+          détails concernant les conditions de résiliation (comme le préavis à respecter ou les frais éventuels).
+        </li>
+        <li>
+          Vous donnez votre nom, vos coordonnées électroniques ou postales, la référence de votre contrat, ou vous confirmez ces
+          informations si elles sont déjà pré-remplies par le professionnel.
+        </li>
+        <li>
+          Si vous résiliez votre contrat avant son terme, vous expliquez pourquoi, et si besoin, vous envoyez les justificatifs
+          (par exemple, pour les contrats d'accès à Internet, vous pouvez résilier gratuitement en cas de surendettement).
+        </li>
+        <li>
+          Ensuite, vous arrivez sur une page récapitulative où vous pouvez vérifier toutes les informations que vous avez
+          fournies.
+        </li>
+        <li>
+          Une fois que tout est en ordre, vous validez pour finaliser la démarche et vous notifiez votre résiliation au
+          professionnel en cliquant sur le bouton "notification de la résiliation".
+        </li>
+        <li>
+          L'entreprise doit vous confirmer qu'elle a bien reçu votre demande de résiliation, puis elle doit vous informer dans un
+          délai raisonnable de la date à laquelle votre résiliation prendra effet.
+        </li>
+      </ul>
       <p className="italic">
         Consulter{' '}
         <Link href="https://www.legifrance.gouv.fr/jorf/id/JORFTEXT000047314374" target="_blank">
