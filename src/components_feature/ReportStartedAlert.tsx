827e8d01e8a4e32bd0388463dbf64180824d9622
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2023-10-03 10:01:58 +0200

    accessibility : use DSFR stepper

--- src/components_feature/ReportStartedAlert.tsx
+++ src/components_feature/ReportStartedAlert.tsx
@@ -1,13 +1,11 @@
 import {Button} from '@codegouvfr/react-dsfr/Button'
 import {Box, Card, Slide} from '@mui/material'
-import {AnomalyImage} from 'components_simple/AnomalyImage'
 import {buildPathForStep} from 'components_feature/reportFlow/reportFlowStepper/ReportFlowStepper'
 import {ReportFlowStepperHeader} from 'components_feature/reportFlow/reportFlowStepper/ReportFlowStepperHeader'
 import {useI18n} from 'i18n/I18n'
 import {findCurrentStepForReport} from 'model/ReportStep'
 import Link from 'next/link'
 import {useMemo} from 'react'
-import {Txt} from '../components_simple/Txt'
 import {useReportFlowContext} from './reportFlow/ReportFlowContext'
 
 export default function ReportStartedAlert() {
@@ -23,7 +21,6 @@ export default function ReportStartedAlert() {
           sx={{
             border: t => `2px solid ${t.palette.primary.main}`,
             p: 2,
-            display: 'flex',
             bottom: 20,
             right: 20,
             background: 'white',
@@ -31,13 +28,14 @@ export default function ReportStartedAlert() {
             position: 'fixed',
           }}
         >
-          <AnomalyImage anomaly={_report.reportDraft.anomaly} scale={0.8} sx={{mr: 1}} />
           <Box>
-            <h2 className="font-medium text-lg mb-0">{m.continueReport}</h2>
-            <Txt block color="hint">
-              {_report.reportDraft.anomaly.title}
-            </Txt>
-            <ReportFlowStepperHeader variant="report-started-alert" currentStep={currentStep} />
+            <h2 className="fr-h5 !mb-2">{m.continueReport}</h2>
+            <ReportFlowStepperHeader
+              isWebView={false}
+              step={currentStep}
+              variant="report-started-alert"
+              anomalyTitle={_report.reportDraft.anomaly.title}
+            />
             <div className="flex justify-end gap-4">
               <Button size="small" priority="tertiary" onClick={_report.resetFlow}>
                 {m.delete}
