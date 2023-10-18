c03bd4e64ced8ccd117a4686fb8a6bf3c9c15645
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2023-10-18 11:39:40 +0200

    accessibility: use Button in SwitchLang

--- src/components_simple/SwitchLang.tsx
+++ src/components_simple/SwitchLang.tsx
@@ -5,28 +5,31 @@ import {usePathname, useRouter} from 'next/navigation'
 import {useReportFlowContext} from '../components_feature/reportFlow/ReportFlowContext'
 import {useI18n} from '../i18n/I18n'
 import {replaceLangInPath, switchLang} from '../i18n/I18nTools'
+import Button from '@codegouvfr/react-dsfr/Button'
 
 export function SwitchLang() {
   const pathname = usePathname()
   const router = useRouter()
   const _report = useReportFlowContext()
   const {m, currentLang} = useI18n()
+  const path = replaceLangInPath(pathname, switchLang(currentLang))
   return (
-    <a
-      href="#"
-      className="fr-btn fr-btn--tertiary"
-      onClick={e => {
-        e.preventDefault()
-        //Reset report on lang switch
-        _report.resetFlow()
-        const path = replaceLangInPath(pathname, switchLang(currentLang))
-        setCookie('NEXT_LANG', switchLang(currentLang))
-        router.push(path)
+    <Button
+      linkProps={{
+        href: path,
+        onClick: e => {
+          e.preventDefault()
+          //Reset report on lang switch
+          _report.resetFlow()
+          setCookie('NEXT_LANG', switchLang(currentLang))
+          router.push(path)
+        },
+        title: m.header.selectLang,
       }}
-      title={m.header.selectLang}
+      priority="tertiary"
     >
       {m.header.currentLangCode}
       <span className="fr-hidden-lg"> - {m.header.currentLang}</span>
-    </a>
+    </Button>
   )
 }
