aed3c9ae97cb6e8d5c67b6553936dc2b45a88511
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2023-11-08 15:40:27 +0100

    Remove reference Rugby World Cup on homepage EN

--- src/components_simple/bigBanners/ForeignVisitorsQaPromoBanner.tsx
+++ src/components_simple/bigBanners/ForeignVisitorsQaPromoBanner.tsx
@@ -2,7 +2,7 @@ export function ForeignVisitorsQaPromoBanner() {
   return (
     <div className="mt-6 py-4 px-4 mx-auto border bxorder-l-8 border-gray-500 border-solid ">
       If you have a question about tickets, flights, hotels, restaurants, transport, payments, etc. regarding your stay in France
-      for the Rugby World Cup 2023 or the Paris 2024 Olympic Games, your answer might be here :{' '}
+      for the Paris 2024 Olympic Games, your answer might be here :{' '}
       <a
         href="https://www.economie.gouv.fr/dgccrf/2023-rugby-world-cup-and-paris-2024-olympic-and-paralympic-games-consumer-questions"
         rel="noreferrer"
