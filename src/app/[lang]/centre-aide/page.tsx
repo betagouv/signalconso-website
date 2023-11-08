473d5661b399514f150fcdd0de5cfc33e4fc040e
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2023-11-08 09:29:15 +0100

    Remove unused centre d aide

--- src/app/[lang]/centre-aide/page.tsx
+++
@@ -1,6 +0,0 @@
-import {buildGenerateMetadataForNoIndexPage} from 'core/metadatas'
-import {CentreAide} from 'reusablePages/centre-aide'
-
-export const generateMetadata = buildGenerateMetadataForNoIndexPage('aide')
-
-export default CentreAide
