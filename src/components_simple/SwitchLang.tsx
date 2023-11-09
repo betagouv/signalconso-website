13e56e3e176b16bc7a89c6c9b7abb85c2ee7c1d2
ssedoudbgouv <s.sedoud.betagouv@gmail.com>
2023-11-09 10:06:24 +0100

    TRELLO-2058: fix switch lang issue

--- src/components_simple/SwitchLang.tsx
+++ src/components_simple/SwitchLang.tsx
@@ -5,31 +5,37 @@ import {usePathname, useRouter} from 'next/navigation'
 import {useReportFlowContext} from '../components_feature/reportFlow/ReportFlowContext'
 import {useI18n} from '../i18n/I18n'
 import {replaceLangInPath, switchLang} from '../i18n/I18nTools'
-import Button from '@codegouvfr/react-dsfr/Button'
+import {internalPageDefs} from '../core/pagesDefinitions'
+import Link from 'next/link'
 
 export function SwitchLang() {
   const pathname = usePathname()
   const router = useRouter()
-  const _report = useReportFlowContext()
   const {m, currentLang} = useI18n()
   const path = replaceLangInPath(pathname, switchLang(currentLang))
+  const home = `/${switchLang(currentLang)}`
+
+  const newPath = () => {
+    const hasAlternatePageInOtherLang = Object.values(internalPageDefs).find(_ => {
+      return _.url != '/' && pathname.includes(_.url)
+    })
+
+    return hasAlternatePageInOtherLang ? path : home
+  }
+
   return (
-    <Button
-      linkProps={{
-        href: path,
-        onClick: e => {
-          e.preventDefault()
-          //Reset report on lang switch
-          _report.resetFlow()
-          setCookie('NEXT_LANG', switchLang(currentLang))
-          router.push(path)
-        },
-        title: m.header.selectLang,
+    <Link
+      className={'fr-btn fr-btn--tertiary'}
+      href={newPath()}
+      onClick={e => {
+        const p = newPath()
+        e.preventDefault()
+        setCookie('NEXT_LANG', switchLang(currentLang))
+        router.push(p)
       }}
-      priority="tertiary"
     >
       {m.header.currentLangCode}
       <span className="fr-hidden-lg"> - {m.header.currentLang}</span>
-    </Button>
+    </Link>
   )
 }
