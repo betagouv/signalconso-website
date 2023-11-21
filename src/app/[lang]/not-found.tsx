608c09c3c597af356638833ead62332a8ea00f9a
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2023-11-21 13:00:17 +0100

    remove duplicated things from not-found

--- src/app/[lang]/not-found.tsx
+++ src/app/[lang]/not-found.tsx
@@ -1,11 +1,9 @@
 'use client'
-import {DsfrProvider} from '@codegouvfr/react-dsfr/next-appdir/DsfrProvider'
 import {usePathname} from 'next/navigation'
 import {LinkBackToHome} from '../../components_simple/LinkBackToHome'
 import '../../globals.css'
 import {getI18n} from '../../i18n/I18nDictionnary'
 import {AppLangs, getSupportedLang} from '../../i18n/localization/AppLangs'
-import ThemeSetup from './ThemeSetup'
 
 // No metadata here, not available yet https://github.com/vercel/next.js/issues/45620
 export default function NotFound() {
@@ -17,18 +15,14 @@ export default function NotFound() {
   const {messages: m} = getI18n(lang)
 
   return (
-    <ThemeSetup>
-      <DsfrProvider lang={lang}>
-        <main role="main" id="main-content">
-          <div className="fr-container">
-            <div className="py-20 max-w-md mx-auto">
-              <h1>{m.pageNotFoundTitle}</h1>
-              <p>{m.pageNotFoundDesc}</p>
-              <LinkBackToHome isWebView={isWebView} lang={lang} />
-            </div>
-          </div>
-        </main>
-      </DsfrProvider>
-    </ThemeSetup>
+    <main role="main" id="main-content">
+      <div className="fr-container">
+        <div className="py-20 max-w-md mx-auto">
+          <h1>{m.pageNotFoundTitle}</h1>
+          <p>{m.pageNotFoundDesc}</p>
+          <LinkBackToHome isWebView={isWebView} lang={lang} />
+        </div>
+      </div>
+    </main>
   )
 }
