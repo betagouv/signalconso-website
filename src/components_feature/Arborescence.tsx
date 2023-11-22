3d0a02898c3c59db3753cee1cff0222b2ed0572b
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2023-11-22 11:30:35 +0100

    remove some MUI stuff, mostly in Arborescence

--- src/components_feature/Arborescence.tsx
+++ src/components_feature/Arborescence.tsx
@@ -1,11 +1,8 @@
 'use client'
 
-import Button from '@codegouvfr/react-dsfr/Button'
-import {Box, Checkbox, IconButton, Radio, useTheme} from '@mui/material'
-import Icon from '@mui/material/Icon'
 import {getOptionsFromInput, getPlaceholderFromInput} from '@/components_feature/reportFlow/Details/DetailInputsUtils'
 import {ContentPageContainer} from '@/components_simple/PageContainers'
-import {styleUtils} from '@/core/theme'
+import {Button} from '@codegouvfr/react-dsfr/Button'
 import {useEffect, useState} from 'react'
 import {
   allVisibleAnomalies,
@@ -14,15 +11,10 @@ import {
   instanceOfSubcategoryWithInputs,
 } from '../anomalies/Anomalies'
 import {Anomaly, DetailInputType, StandardSubcategory, Subcategory, SubcategoryWithInfoWall} from '../anomalies/Anomaly'
-import {Txt} from '../components_simple/Txt'
 import {useI18n} from '../i18n/I18n'
 import {fnSwitch} from '../utils/FnSwitch'
 
 const Node = ({anomaly, open}: {anomaly: Anomaly | Subcategory; open?: boolean}) => {
-  const iconWidth = 40
-  const iconMargin = 8
-  const theme = useTheme()
-
   const title = instanceOfAnomaly(anomaly) ? anomaly.category : anomaly.title
   const [isOpen, setIsOpen] = useState(false)
   useEffect(() => {
@@ -30,166 +22,123 @@ const Node = ({anomaly, open}: {anomaly: Anomaly | Subcategory; open?: boolean})
   }, [open])
 
   return (
-    <Box sx={{display: 'flex', alignItems: 'flex-start', mb: 2}}>
-      <Box
-        sx={{
-          width: iconWidth,
-          marginRight: `${iconMargin}px`,
-        }}
-      >
+    <div className="flex items-start mb-4">
+      <div className="w-[40px] mr-[8px]">
         {anomaly.subcategories ? (
-          <IconButton color="primary" onClick={() => setIsOpen(_ => !_)}>
-            <Icon>{isOpen ? 'keyboard_arrow_down' : 'keyboard_arrow_right'}</Icon>
-          </IconButton>
+          <button
+            onClick={() => setIsOpen(_ => !_)}
+            className="bg-sclightpurple h-[40px] w-[40px] text-scbluefrance flex items-center ri-arrow-down-s-line justify-center"
+          ></button>
         ) : (
-          <IconButton disabled>
-            <Icon>remove</Icon>
-          </IconButton>
+          <i className="ri-file-line text-gray-500 mx-2" />
         )}
-      </Box>
-      <Box>
-        <Box sx={{minHeight: 42, display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
-          <Txt block>
-            <span dangerouslySetInnerHTML={{__html: title}} /> <Txt color="hint">{anomaly.id}</Txt>
-          </Txt>
+      </div>
+      <div>
+        <div className="min-h-[42px] flex flex-col justify-center">
+          <p className="mb-0">
+            <span dangerouslySetInnerHTML={{__html: title}} /> <span className="text-gray-500">{anomaly.id}</span>
+          </p>
           {instanceOfAnomaly(anomaly) && anomaly.description && (
-            <Txt block size="small" color="hint" dangerouslySetInnerHTML={{__html: anomaly.description}} />
+            <p className="text-sm text-gray-500 mb-0" dangerouslySetInnerHTML={{__html: anomaly.description}} />
           )}
           {(anomaly as Subcategory).reponseconsoCode && (
-            <Txt key={1} size="small" color="hint">
+            <span key={1} className="text-sm text-gray-500">
               <b>ReponseConso codes:</b>&nbsp;
               {(anomaly as Subcategory).reponseconsoCode?.join(', ')}
-            </Txt>
+            </span>
           )}
-          <Box>
+          <div>
             {(anomaly as Subcategory).tags?.map(tag => (
-              <Box
-                sx={{
-                  mr: 1,
-                  borderRadius: 100,
-                  height: 26,
-                  px: 1.5,
-                  backgroundColor: t => t.palette.action.disabledBackground,
-                  display: 'inline-flex',
-                  alignItems: 'center',
-                  fontSize: styleUtils(theme).fontSize.small,
-                  // fontWeight: t => t.typography.fontWeightBold,
-                  color: t => t.palette.text.secondary,
-                }}
-                key={tag}
-              >
+              <div className="mr-2 rounded-lg h-[26px] px-2 bg-gray-300 inline-flex items-center text-sm text-gray-800" key={tag}>
                 {tag}
-              </Box>
+              </div>
             ))}
-          </Box>
+          </div>
           {instanceOfSubcategoryWithInfoWall(anomaly) && <NodeInfo anomaly={anomaly} />}
           {instanceOfSubcategoryWithInputs(anomaly) && <NodeInput anomaly={anomaly} />}
-        </Box>
+        </div>
         {isOpen && anomaly.subcategories && (
-          <Box
-            sx={{
-              my: 1,
-              position: 'relative',
-              '&:before': {
-                content: "' '",
-                height: '100%',
-                width: '1px',
-                position: 'absolute',
-                background: t => t.palette.divider,
-                left: -iconWidth / 2 - iconMargin,
-                // borderLeft: t => `1px solid ${t.palette.divider}`
-              },
-            }}
-          >
+          <div className="my-2 relative before:h-full before:content-['_'] before:w-[1px] before:absolute before:bg-gray-500 before:left-[-28px]">
             {anomaly.subcategories.map(s => (
               <Node open={open} key={s.id} anomaly={s} />
             ))}
-          </Box>
+          </div>
         )}
-      </Box>
-    </Box>
+      </div>
+    </div>
   )
 }
 
 const NodeInput = ({anomaly}: {anomaly: StandardSubcategory}) => {
-  return (
-    <>
-      {anomaly.detailInputs?.map(input => (
-        <Box key={input.label}>
-          <Txt block sx={{mt: 1}} size="small" color="hint" dangerouslySetInnerHTML={{__html: input.label}} />
-          {fnSwitch(
-            input.type,
-            {
-              [DetailInputType.RADIO]: () => (
-                <>
-                  {getOptionsFromInput(input)!.map(option => (
-                    <Box key={option} sx={{display: 'flex', alignItems: 'center'}}>
-                      <Radio disabled size="small" sx={{mr: 1, p: 1 / 4}} />
-                      <Txt size="small">{option}</Txt>
-                    </Box>
-                  ))}
-                </>
+  if (anomaly.detailInputs && anomaly.detailInputs.length > 0) {
+    return (
+      <div className="p-2 my-2 border-gray-300 border-solid border rounded-lg">
+        {anomaly.detailInputs?.map(input => (
+          <div key={input.label}>
+            <p className="mb-0 mt-2 text-sm text-gray-600" dangerouslySetInnerHTML={{__html: input.label}} />
+            {fnSwitch(
+              input.type,
+              {
+                [DetailInputType.RADIO]: () => (
+                  <>
+                    {getOptionsFromInput(input)!.map(option => (
+                      <div key={option} className="flex items-center">
+                        <i className="ri-checkbox-blank-circle-line mx-2" />
+                        <span className="text-sm">{option}</span>
+                      </div>
+                    ))}
+                  </>
+                ),
+                [DetailInputType.CHECKBOX]: () => (
+                  <>
+                    {getOptionsFromInput(input)!.map(option => (
+                      <div key={option} className="flex items-center">
+                        <i className="ri-checkbox-blank-line mx-2" />
+                        <span className="text-sm">{option}</span>
+                      </div>
+                    ))}
+                  </>
+                ),
+              },
+              () => (
+                <div className="px-2 text-gray-500 mx-2 bg-gray-100 h-[32px] w-[200px] border-gray-500 border-solid border">
+                  {getPlaceholderFromInput(input) ?? ''}
+                </div>
               ),
-              [DetailInputType.CHECKBOX]: () => (
-                <>
-                  {getOptionsFromInput(input)!.map(option => (
-                    <Box key={option} sx={{display: 'flex', alignItems: 'center'}}>
-                      <Checkbox disabled size="small" sx={{mr: 1, p: 1 / 4}} />
-                      <Txt size="small">{option}</Txt>
-                    </Box>
-                  ))}
-                </>
-              ),
-            },
-            () => (
-              <Box
-                sx={{
-                  px: 1,
-                  color: t => t.palette.text.disabled,
-                  borderRadius: 4,
-                  height: 32,
-                  width: 200,
-                  border: t => `1px solid ${t.palette.divider}`,
-                }}
-              >
-                {getPlaceholderFromInput(input) ?? '...'}
-              </Box>
-            ),
-          )}
-        </Box>
-      ))}
-    </>
-  )
+            )}
+          </div>
+        ))}
+      </div>
+    )
+  }
+  return null
 }
 
 const NodeInfo = ({anomaly}: {anomaly: SubcategoryWithInfoWall}) => {
   const {m} = useI18n()
   return (
-    <>
+    <div className="border border-solid border-gray-300 rounded-lg my-2 p-2 ">
+      <p className=" bg-gray-500 text-white italic px-2 text-center">(information bloquante)</p>
       {anomaly.blockingInfo.title && <div dangerouslySetInnerHTML={{__html: anomaly.blockingInfo.title}} />}
       {anomaly.blockingInfo.subTitle && <div dangerouslySetInnerHTML={{__html: anomaly.blockingInfo.subTitle}} />}
       {anomaly.blockingInfo.content && (
-        <Txt color="hint" size="small" dangerouslySetInnerHTML={{__html: anomaly.blockingInfo.content}} />
+        <span className="text-sm text-gray-500" dangerouslySetInnerHTML={{__html: anomaly.blockingInfo.content}} />
       )}
       {anomaly.blockingInfo.questions && (
-        <Txt color="hint" size="small">
+        <div className="text-sm text-gray-500 ">
           <ul>
             {anomaly.blockingInfo.questions.map(action => (
               <li key={action.question}>
-                <Txt bold dangerouslySetInnerHTML={{__html: action.question}} />
+                <span className="font-bold" dangerouslySetInnerHTML={{__html: action.question}} />
                 {action.desc && <div dangerouslySetInnerHTML={{__html: action.desc}} />}
                 <div dangerouslySetInnerHTML={{__html: action.answer}} />
               </li>
             ))}
           </ul>
-        </Txt>
+        </div>
       )}
-      {anomaly.blockingInfo.notAFraudMessage && (
-        <Txt color="hint" size="small">
-          {m.arbo.notAFraudMessage}
-        </Txt>
-      )}
-    </>
+      {anomaly.blockingInfo.notAFraudMessage && <span className="text-sm text-gray-500">{m.arbo.notAFraudMessage}</span>}
+    </div>
   )
 }
 
