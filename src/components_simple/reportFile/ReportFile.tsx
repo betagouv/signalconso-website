0bba726e35cc7ad9df29405ae86102bf63762fd8
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2023-11-29 13:16:17 +0100

    remove more MUI (not tooltip)

--- src/components_simple/reportFile/ReportFile.tsx
+++ src/components_simple/reportFile/ReportFile.tsx
@@ -1,7 +1,6 @@
 import {useApiClients} from '@/context/ApiClientsContext'
 import {useI18n} from '@/i18n/I18n'
 import {Button} from '@codegouvfr/react-dsfr/Button'
-import {Tooltip} from '@mui/material'
 import {UploadedFile} from '../../model/UploadedFile'
 import {ScDialog} from '../ScDialog'
 import {FileType, extensionToType} from './reportFileConfig'
@@ -24,62 +23,63 @@ export const ReportFile = ({file, onRemove}: ReportFileProps) => {
   }
 
   return (
-    <Tooltip title={file.filename}>
-      <div className="mb-4 ml-4">
-        <a
-          target="_blank"
-          href={fileUrl}
-          rel="noreferrer"
-          className="after:!hidden !bg-none block relative"
-          aria-label={`Télécharger ${file.filename}`}
-        >
-          <div className="inline-flex border border-solid border-gray-500 overflow-hidden rounded h-[100px] w-[100px] relative">
-            <div className="flex items-center justify-center bg-cover h-full w-full">
-              {(() => {
-                switch (fileType) {
-                  case FileType.Image:
-                    return (
-                      <>
-                        <Thumbnail src={fileUrl} />
-                        <i className="ri-image-line text-gray-500" />
-                      </>
-                    )
-                  case FileType.Doc:
-                    return <i className="ri-file-text-line text-gray-500" />
-                  case FileType.PDF:
-                  default:
-                    return <i className="ri-file-3-line text-gray-500" />
-                }
-              })()}
-            </div>
+    <div className="mb-4 ml-4">
+      <a
+        target="_blank"
+        href={fileUrl}
+        rel="noreferrer"
+        className="after:!hidden !bg-none relative flex flex-col"
+        aria-label={`Télécharger ${file.filename}`}
+      >
+        <div className="inline-flex border border-solid border-gray-500 overflow-hidden rounded h-[100px] w-[100px] relative">
+          <div className="flex items-center justify-center bg-cover h-full w-full">
+            {(() => {
+              switch (fileType) {
+                case FileType.Image:
+                  return (
+                    <>
+                      <Thumbnail src={fileUrl} />
+                      <i className="ri-image-line text-gray-500" />
+                    </>
+                  )
+                case FileType.Doc:
+                  return <i className="ri-file-text-line text-gray-500" />
+                case FileType.PDF:
+                default:
+                  return <i className="ri-file-3-line text-gray-500" />
+              }
+            })()}
           </div>
-        </a>
-        {onRemove && (
-          <div className="w-full text-center">
-            <ScDialog
-              title={m.removeAsk}
-              iconId={'fr-icon-delete-line'}
-              content={<p className="mb-0" dangerouslySetInnerHTML={{__html: m.thisWillBeRemoved(file.filename)}} />}
-              onConfirm={close => {
-                remove()
-                close()
-              }}
-              confirmLabel={m.delete}
+        </div>
+        <span className="px-1 w-[100px] text-black text-center text-sm whitespace-nowrap overflow-hidden text-ellipsis">
+          {file.filename}
+        </span>
+      </a>
+      {onRemove && (
+        <div className="w-full text-center">
+          <ScDialog
+            title={m.removeAsk}
+            iconId={'fr-icon-delete-line'}
+            content={<p className="mb-0" dangerouslySetInnerHTML={{__html: m.thisWillBeRemoved(file.filename)}} />}
+            onConfirm={close => {
+              remove()
+              close()
+            }}
+            confirmLabel={m.delete}
+          >
+            <Button
+              size="small"
+              iconId="fr-icon-delete-line"
+              priority="tertiary no outline"
+              className="!p-0"
+              nativeButtonProps={{'aria-label': `${m.delete} ${file.filename}`}}
             >
-              <Button
-                size="small"
-                iconId="fr-icon-delete-line"
-                priority="tertiary no outline"
-                className="!p-0"
-                nativeButtonProps={{'aria-label': `${m.delete} ${file.filename}`}}
-              >
-                {m.delete.toLowerCase()}
-              </Button>
-            </ScDialog>
-          </div>
-        )}
-      </div>
-    </Tooltip>
+              {m.delete.toLowerCase()}
+            </Button>
+          </ScDialog>
+        </div>
+      )}
+    </div>
   )
 }
 
@@ -87,7 +87,7 @@ function Thumbnail({src}: {src: string}) {
   const loaded = useImageLoaderWithRetries(src)
   return (
     <div
-      className="absolute top-0 right-0 left-0 bottom-0 bg-cover rounded-sm"
+      className="absolute top-0 right-0 left-0 bottom-0 bg-cover rounded-sm z-10"
       {...(loaded ? {style: {backgroundImage: `url("${src}")`}} : null)}
     />
   )
