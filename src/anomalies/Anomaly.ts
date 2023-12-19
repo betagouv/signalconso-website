a96f25c4fe3323529cc8800303662ba506e460bf
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2023-12-19 14:26:30 +0100

    Création de accessbilité

--- src/anomalies/Anomaly.ts
+++ src/anomalies/Anomaly.ts
@@ -92,12 +92,43 @@ export const companyKinds = [
   'LOCATION',
   'SOCIAL',
   'PRODUCT',
+  'TRAIN',
 ] as const
 export type CompanyKinds = (typeof companyKinds)[number]
 export type SpecificWebsiteCompanyKinds = Extract<CompanyKinds, 'MERCHANT_WEBSITE' | 'TRANSPORTER_WEBSITE'>
 
 export const socialNetworks = ['SNAPCHAT', 'INSTAGRAM', 'FACEBOOK', 'YOUTUBE', 'TIKTOK', 'TWITTER', 'TWITCH', 'LINKEDIN'] as const
 export type SocialNetworks = (typeof socialNetworks)[number]
+
+export const trains = [
+  'INOUI_INTERCITES',
+  'OUIGO',
+  'TER',
+  'TRANSILIEN',
+  'EUROSTAR',
+  'TGV_LYRIA',
+  'TGV_ITALIE',
+  'TRENITALIA',
+  'RENFE',
+  'ICE',
+  'TRAIN_DE_NUIT',
+] as const
+export type Trains = (typeof trains)[number]
+
+export const ters = [
+  'AUVERGNE_RHONE_ALPES',
+  'BOURGOGNE_FRANCHE_COMTE',
+  'BRETAGNE',
+  'CENTRE_VAL_DE_LOIRE',
+  'GRAND_EST',
+  'HAUTS_DE_FRANCE',
+  'NOUVELLE_AQUITAINE',
+  'NORMANDIE',
+  'OCCITANIE',
+  'PACA',
+  'PAYS_DE_LA_LOIRE',
+] as const
+export type Ters = (typeof ters)[number]
 
 export interface InfoWall {
   title?: string
