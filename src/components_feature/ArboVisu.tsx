f5a6aa9da69474d982198c0256f006daf5832faf
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2024-01-02 11:49:14 +0100

    arbo visu suite

--- src/components_feature/ArboVisu.tsx
+++ src/components_feature/ArboVisu.tsx
@@ -1,27 +1,30 @@
 'use client'
 
 import * as Plot from '@observablehq/plot'
-import React, {useEffect, useRef, useState} from 'react'
+import React, {useEffect, useMemo, useRef, useState} from 'react'
 import {createElement as h} from 'react'
 import * as d3 from 'd3'
+import {allVisibleAnomalies} from '@/anomalies/Anomalies'
+import {Subcategory} from '@/anomalies/Anomaly'
 
 export function ArboVisu() {
   const containerRef = useRef<HTMLDivElement>()
-  const data = aapl
+  const data = useMemo(prepareData, [])
 
   useEffect(() => {
     if (data) {
       const plot = Plot.plot({
         axis: null,
-        height: 100,
+        height: 2000,
+        width: 1000,
         margin: 10,
         marginLeft: 60,
-        marginRight: 120,
+        marginRight: 200,
         marks: [
-          Plot.cluster(gods, {
+          Plot.cluster(data, {
             // textStroke: 'black'.
             // textLayout: 'mirrored',
-            fontSize: '1.5em',
+            fontSize: '1.2em',
           }),
         ],
       })
@@ -41,4 +44,30 @@ const aapl = [
   {Date: new Date('2013-05-17'), Open: 62.721428, High: 62.869999, Low: 61.572857, Close: 61.894287, Volume: 106976100},
 ]
 
-const gods = ['Chaos/Gaia/Mountains', 'Chaos/Gaia/Pontus', 'Chaos/Gaia/Uranus', 'Chaos/Eros', 'Chaos/Erebus', 'Chaos/Tartarus']
+function prepareData() {
+  const anomalies = allVisibleAnomalies('fr')[0].subcategories
+  let paths: string[] = []
+  anomalies.map(ano => {
+    ano.subcategories?.map(ano2 => {
+      paths.push(buildPath(ano, ano2))
+      ano2.subcategories?.map(ano3 => {
+        paths.push(buildPath(ano, ano2, ano3))
+      })
+    })
+  })
+  return paths
+}
+
+const gods = [
+  'Chaos/Eros',
+  'Chaos/Erebus',
+  'Chaos/Tartarus',
+  'Chaos/Gaia/Mountains',
+  'Chaos/Gaia/Pontus',
+  'Chaos/Gaia/Uranus',
+  'other/taf',
+]
+
+function buildPath(...subcats: {title: string}[]) {
+  return subcats.map(_ => _.title.replaceAll(`/`, '_')).join('/')
+}
