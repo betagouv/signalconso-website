b6dbaeefb24495b1289229eca3dca44e54e3cdf2
hakim-sedoud <128887036+hakim-sedoud@users.noreply.github.com>
2024-01-04 15:57:30 +0100

    Trello 2116 Navigation par liens dans le playground (#614)
    * Updated URLs for Each Tab
    
    * Persist URL State on Page Refresh
    
    * Persist URL State on Page Refresh_2
    
    * Create link in playground
    
    * Implement toggle between link list and component view with addition of back button
    
    * Code and visual enhancements
    
    * Code and visual enhancements_2
    
    * Code and visual enhancements_3
    
    * Code and visual enhancements_4
    
    ---------
    
    Co-authored-by: hakim sedoud <hakim.sedou@gmail.com>

--- src/components_feature/playgroundComponents/PlaygroundPage.tsx
+++ src/components_feature/playgroundComponents/PlaygroundPage.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import {Tabs} from '@codegouvfr/react-dsfr/Tabs'
 import {PlaygroundAcknowledgment} from '@/components_feature/playgroundComponents/PlaygroundAcknoledgment'
 import {PlaygroundCompany} from '@/components_feature/playgroundComponents/PlaygroundCompany'
 import {PlaygroundConfirmation, dummyStepNavigation} from '@/components_feature/playgroundComponents/PlaygroundConfirmation'
@@ -9,6 +8,11 @@ import {PlaygroundDetails} from '@/components_feature/playgroundComponents/Playg
 import {PlaygroundOther} from '@/components_feature/playgroundComponents/PlaygroundOther'
 import {CompanyFilled} from '@/components_feature/reportFlow/Company/Company'
 import {ContentPageContainer} from '@/components_simple/PageContainers'
+
+import {usePathname, useSearchParams} from 'next/navigation'
+import {AcknowledgmentCases} from '../reportFlow/Acknowledgement/Acknowledgement'
+import Button from '@codegouvfr/react-dsfr/Button'
+import Link from 'next/link'
 
 const companyDraft = {
   id: 'id12345',
@@ -27,23 +31,132 @@ const companyDraft = {
   isOpen: true,
 }
 
+const generalTestCases = ['details', 'companyFilled', 'consumer', 'confirmation', 'other'] as const
+
+const companyTestCases = [
+  'company_siret',
+  'company_website',
+  'company_merchant_website',
+  'company_transporter_website',
+  'company_phone',
+  'company_location',
+  'company_social',
+  'company_product',
+] as const
+
+const acknowledgmentTestCases = [
+  'acknowledgment_ReponseConso',
+  'acknowledgment_EmployeeReport',
+  'acknowledgment_ForeignCompany_Espagne',
+  'acknowledgment_ForeignCompany_Suisse',
+  'acknowledgment_ForeignCompany_Andorre',
+  'acknowledgment_ForeignCompany_Argentine',
+  'acknowledgment_FrenchCompanyWithoutSIRET',
+  'acknowledgment_ContractualDisputeWithSIRET',
+  'acknowledgment_Default',
+] as const
+
+export const playgroundTestCases = [...generalTestCases, ...companyTestCases, ...acknowledgmentTestCases]
+export type PlaygroundTestCase = (typeof playgroundTestCases)[number]
+
 const Playground = () => {
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+
+  const testCase = searchParams.get('testcase') as PlaygroundTestCase | null
+
+  const renderLinks = (testCases: readonly PlaygroundTestCase[]) => {
+    return testCases.map(testCase => (
+      <li key={testCase} className="list-none cursor-pointer hover:text-blue-500 ">
+        <Link href={`${pathname}?testcase=${testCase}`}> {testCase.replace(/_/g, ' ')} </Link>
+      </li>
+    ))
+  }
+
+  const renderComponent = () => {
+    switch (testCase) {
+      case 'details':
+        return <PlaygroundDetails />
+      case 'company_siret':
+        return <PlaygroundCompany companyKind="SIRET" />
+      case 'company_website':
+        return <PlaygroundCompany companyKind="WEBSITE" />
+      case 'company_merchant_website':
+        return <PlaygroundCompany companyKind="MERCHANT_WEBSITE" />
+      case 'company_transporter_website':
+        return <PlaygroundCompany companyKind="TRANSPORTER_WEBSITE" />
+      case 'company_phone':
+        return <PlaygroundCompany companyKind="PHONE" />
+      case 'company_location':
+        return <PlaygroundCompany companyKind="LOCATION" />
+      case 'company_social':
+        return <PlaygroundCompany companyKind="SOCIAL" />
+      case 'company_product':
+        return <PlaygroundCompany companyKind="PRODUCT" />
+      case 'companyFilled':
+        return <CompanyFilled draft={{companyDraft}} onClear={console.log} stepNavigation={dummyStepNavigation} />
+      case 'consumer':
+        return <PlaygroundConsumer />
+      case 'confirmation':
+        return <PlaygroundConfirmation />
+      case 'acknowledgment_ReponseConso':
+        return <PlaygroundAcknowledgment acknowledgmentCase={AcknowledgmentCases.ReponseConso} countryId="" />
+      case 'acknowledgment_EmployeeReport':
+        return <PlaygroundAcknowledgment acknowledgmentCase={AcknowledgmentCases.EmployeeReport} countryId="" />
+      case 'acknowledgment_ForeignCompany_Espagne':
+        return <PlaygroundAcknowledgment acknowledgmentCase={AcknowledgmentCases.ForeignCompany} countryId="ES" />
+      case 'acknowledgment_ForeignCompany_Suisse':
+        return <PlaygroundAcknowledgment acknowledgmentCase={AcknowledgmentCases.ForeignCompany} countryId="CH" />
+      case 'acknowledgment_ForeignCompany_Andorre':
+        return <PlaygroundAcknowledgment acknowledgmentCase={AcknowledgmentCases.ForeignCompany} countryId="AD" />
+      case 'acknowledgment_ForeignCompany_Argentine':
+        return <PlaygroundAcknowledgment acknowledgmentCase={AcknowledgmentCases.ForeignCompany} countryId="AR" />
+      case 'acknowledgment_FrenchCompanyWithoutSIRET':
+        return <PlaygroundAcknowledgment acknowledgmentCase={AcknowledgmentCases.FrenchCompanyWithoutSIRET} countryId="" />
+      case 'acknowledgment_ContractualDisputeWithSIRET':
+        return <PlaygroundAcknowledgment acknowledgmentCase={AcknowledgmentCases.ContractualDisputeWithSIRET} countryId="" />
+      case 'acknowledgment_Default':
+        return <PlaygroundAcknowledgment acknowledgmentCase={AcknowledgmentCases.Default} countryId="" />
+      case 'other':
+        return <PlaygroundOther />
+      default:
+        return null
+    }
+  }
+
   return (
     <ContentPageContainer>
-      <Tabs
-        tabs={[
-          {label: 'details', content: <PlaygroundDetails />},
-          {label: 'company', content: <PlaygroundCompany />},
-          {
-            label: 'companyFilled',
-            content: <CompanyFilled draft={{companyDraft}} onClear={console.log} stepNavigation={dummyStepNavigation} />,
-          },
-          {label: 'consumer', content: <PlaygroundConsumer />},
-          {label: 'confirmation', content: <PlaygroundConfirmation />},
-          {label: 'acknowledgment', content: <PlaygroundAcknowledgment />},
-          {label: 'other', content: <PlaygroundOther />},
-        ]}
-      />
+      <h1>Playground</h1>
+
+      {!testCase ? (
+        <div>
+          <p>
+            Les liens ci-dessous permettent de tester les différents composants de l'interface sans avoir à refaire tout le
+            parcours de signalement.
+          </p>
+          <div className="flex mt-4">
+            <div className="flex-1">
+              <h5>Général</h5>
+              <ul>{renderLinks(generalTestCases)}</ul>
+            </div>
+            <div className="flex-1">
+              <h5>Company</h5>
+              <ul>{renderLinks(companyTestCases)}</ul>
+            </div>
+            <div className="flex-1">
+              <h5>Acknowledgment</h5>
+              <ul>{renderLinks(acknowledgmentTestCases)}</ul>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <>
+          <Button linkProps={{href: `${pathname}`}} className="mb-6">
+            Retour
+          </Button>
+          {renderComponent()}
+        </>
+      )}
     </ContentPageContainer>
   )
 }
