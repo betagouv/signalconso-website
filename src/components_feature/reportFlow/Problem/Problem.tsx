51eff6908011c52775227ab967ad5476935f3c32
ssedoudbgouv <s.sedoud.betagouv@gmail.com>
2024-01-08 09:29:34 +0100

    TRELLO-2129 :  fix format

--- src/components_feature/reportFlow/Problem/Problem.tsx
+++ src/components_feature/reportFlow/Problem/Problem.tsx
@@ -79,7 +79,9 @@ export const Problem = ({anomaly, isWebView, stepNavigation}: Props) => {
   const _analytic = useAnalyticContext()
   const {m, currentLang} = useI18n()
   const {reportDraft, setReportDraft, resetFlow, sendReportEvent} = useReportFlowContext()
-  const hasReponseConsoSubcategories = reportDraft.subcategories ? buildTagsFromSubcategories(reportDraft.subcategories).includes('ReponseConso') : false
+  const hasReponseConsoSubcategories = reportDraft.subcategories
+    ? buildTagsFromSubcategories(reportDraft.subcategories).includes('ReponseConso')
+    : false
 
   // reset the draft when switching the root category
   useEffect(() => {
