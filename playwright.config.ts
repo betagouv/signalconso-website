9392604d4ba4b867c9efdcd062041041c733ed8d
ssedoudbgouv <s.sedoud.betagouv@gmail.com>
2024-01-27 10:43:08 +0100

    TRELLO-2178: update browser

--- playwright.config.ts
+++ playwright.config.ts
@@ -36,35 +36,9 @@ export default defineConfig({
       name: 'chromium',
       use: {...devices['Desktop Chrome']},
     },
-
     {
       name: 'firefox',
       use: {...devices['Desktop Firefox']},
-    },
-
-    {
-      name: 'webkit',
-      use: {...devices['Desktop Safari']},
-    },
-
-    /* Test against mobile viewports. */
-    {
-      name: 'Mobile Chrome',
-      use: {...devices['Pixel 8']},
-    },
-    {
-      name: 'Mobile Safari',
-      use: {...devices['iPhone 15']},
-    },
-
-    /* Test against branded browsers. */
-    {
-      name: 'Microsoft Edge',
-      use: {...devices['Desktop Edge'], channel: 'msedge'},
-    },
-    {
-      name: 'Google Chrome',
-      use: {...devices['Desktop Chrome'], channel: 'chrome'},
     },
   ],
 
