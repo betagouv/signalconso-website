2443b536426f957633facbdf225a9b141bdfa1b3
ssedoudbgouv <s.sedoud.betagouv@gmail.com>
2024-02-10 10:57:52 +0100

    TRELLO-2174 : update influencer flow

--- src/components_feature/reportFlow/Company/InfluencerBySocialNetwork.tsx
+++ src/components_feature/reportFlow/Company/InfluencerBySocialNetwork.tsx
@@ -1,5 +1,5 @@
 import {RequiredFieldsLegend} from '@/components_simple/RequiredFieldsLegend'
-import {BtnNextSubmit} from '@/components_simple/buttons/Buttons'
+import {BtnNext, BtnNextSubmit, ButtonWithLoader} from '@/components_simple/buttons/Buttons'
 import {ScTextInput} from '@/components_simple/formInputs/ScTextInput'
 import {Controller, useForm} from 'react-hook-form'
 import {SocialNetworks, socialNetworks} from '../../../anomalies/Anomaly'
@@ -8,14 +8,35 @@ import {SocialNetworkRow} from '../../../components_simple/SocialNetworkRow'
 import {ScRadioButtons} from '../../../components_simple/formInputs/ScRadioButtons'
 import {useI18n} from '../../../i18n/I18n'
 import {DetailsSpecifyInput} from '@/components_feature/reportFlow/Details/DetailsSpecifyInput'
+import {AutofocusedDiv} from "@/components_simple/AutofocusedDiv";
+import {Button} from "@codegouvfr/react-dsfr/Button";
+import {SignalConsoApiClient} from "@/clients/SignalConsoApiClient";
+import {SiretExtractorClient} from "@/clients/SiretExtractorClient";
+import {useQuery, useQueryClient} from "@tanstack/react-query";
+import {useApiClients} from "@/context/ApiClientsContext";
+import {useToastOnQueryError} from "@/clients/apiHooks";
+import {Influencer} from "@/model/Influencer";
+import {useEffect, useState} from "react";
+import {Alert} from "@codegouvfr/react-dsfr/Alert";
 
 interface Props {
   onSubmit: (socialNetwork: SocialNetworks, influencer: string, otherSocialNetwork?: string) => void
 }
+
 interface Form {
   socialNetwork: SocialNetworks
   otherSocialNetwork: string
-  influencer: string
+  influencer: string,
+  certified?: boolean
+}
+
+
+async function searchInfluencer(
+  signalConsoApiClient: SignalConsoApiClient,
+  influencer: string,
+  socialNetwork: SocialNetworks
+): Promise<Influencer[]> {
+  return await signalConsoApiClient.searchCertifiedInfluencer(influencer, socialNetwork)
 }
 
 export const InfluencerBySocialNetwork = ({onSubmit}: Props) => {
@@ -25,22 +46,105 @@ export const InfluencerBySocialNetwork = ({onSubmit}: Props) => {
     watch,
     control,
     register,
+    setValue,
     formState: {errors},
   } = useForm<Form>()
 
+
+
+
+  const {signalConsoApiClient} = useApiClients()
   const socialNetwork = watch('socialNetwork')
+
+  useEffect(() => {
+    setValue("influencer", "")
+    setIsEditingWebsite(true)
+  }, [socialNetwork])
+
+  const influencer = watch('influencer')
+  const [isEditingWebsite, setIsEditingWebsite] = useState(true)
+  const [getCertifiedInfluencer, setGetCertifiedInfluencer] = useState(false)
+
+  const searchQuery = useQuery({
+    queryKey: ['searchCertifiedInfluencer', influencer, socialNetwork],
+    queryFn: () => {
+      return searchInfluencer(signalConsoApiClient, influencer, socialNetwork).then(res => {
+        setGetCertifiedInfluencer(false)
+        return res;
+      })
+    },
+    enabled: getCertifiedInfluencer
+  })
+
+  useToastOnQueryError(searchQuery)
 
   const socialNetworkOptions = socialNetworks.map(socialNetwork => {
     return {
-      label: <SocialNetworkRow socialNetwork={socialNetwork} />,
+      label: <SocialNetworkRow socialNetwork={socialNetwork}/>,
       value: socialNetwork,
-      specify: socialNetwork === 'OTHER' ? <DetailsSpecifyInput control={control} name="otherSocialNetwork" /> : undefined,
+      specify: socialNetwork === 'OTHER' ?
+        <DetailsSpecifyInput control={control} name="otherSocialNetwork"/> : undefined,
     }
   })
 
+
+
+
+  function CertifiedInfluencer({
+                                 currentInfluencer,
+                                 certifiedInfluencers,
+                               }: {
+    currentInfluencer: string,
+    certifiedInfluencers: string[] | undefined,
+
+  }) {
+    const {m} = useI18n()
+    if (certifiedInfluencers && !isEditingWebsite) {
+      return (
+        <AutofocusedDiv>
+          <br/>
+          {(certifiedInfluencers.includes(currentInfluencer)) ?
+            (
+              <>
+                <Alert title={m.influencerIdentifiedTitle}
+                       description={m.influencerIdentifiedDesc}
+                       severity="success" className="text-base font-normal mb-3"/>
+                <div className="flex justify-end">
+                  <Button
+                    type="submit"
+                  >
+                    {m.continueWithInfluencer(currentInfluencer)}
+                  </Button>
+                </div>
+              </>
+
+            ) : (
+              <div className="flex-col">
+                <Alert title={m.influencerUnknownTitle}
+                       description={m.influencerUnknownDesc}
+                       severity="warning" className="text-base font-normal mb-3"/>
+                <div className="flex flex-row justify-end">
+                  <Button priority={"secondary"} className={"mr-2"} onClick={_ => setIsEditingWebsite(true)}>
+                    {m.edit}
+                  </Button>
+                  <Button type="submit">
+                    {m.continueWithWebsite(currentInfluencer)}
+                  </Button>
+                </div>
+              </div>)
+          }
+
+        </AutofocusedDiv>
+
+      )
+    }
+    return null;
+  }
+
+
   return (
     <>
-      <RequiredFieldsLegend />
+      <RequiredFieldsLegend/>
       <form
         onSubmit={handleSubmit(form => {
           onSubmit(form.socialNetwork, form.influencer, form.otherSocialNetwork)
@@ -54,7 +158,8 @@ export const InfluencerBySocialNetwork = ({onSubmit}: Props) => {
               rules={{
                 required: {value: true, message: m.required},
               }}
-              render={({field}) => <ScRadioButtons {...field} required options={socialNetworkOptions} title="Réseau social" />}
+              render={({field}) => <ScRadioButtons {...field} required options={socialNetworkOptions}
+                                                   title="Réseau social"/>}
             />
           </div>
         </Animate>
@@ -68,10 +173,34 @@ export const InfluencerBySocialNetwork = ({onSubmit}: Props) => {
                 placeholder="Nom ou pseudonyme"
                 {...register('influencer', {required: {value: true, message: m.required}})}
                 required
+                editable={!isEditingWebsite ? {
+                    onEdit: () => {
+                      setValue("influencer", "")
+                      setIsEditingWebsite(true)
+                    },
+                    label: m.modifyWebsite,
+                  }
+                  : undefined
+                }
+                disabled={!isEditingWebsite}
               />
-              <div className="flex justify-end">
-                <BtnNextSubmit />
-              </div>
+
+              <CertifiedInfluencer
+                currentInfluencer={influencer}
+                certifiedInfluencers={searchQuery.data ? searchQuery.data.map(_ => _.name) : undefined}
+              />
+
+
+              {isEditingWebsite &&
+                (<div className="flex justify-end">
+                  <ButtonWithLoader loading={searchQuery.isLoading} disabled={influencer == ""} onClick={() => {
+                    setGetCertifiedInfluencer(true)
+                    setIsEditingWebsite(false)
+                  }} iconId={"fr-icon-arrow-right-s-line"}>Next</ButtonWithLoader>
+                </div>)
+
+              }
+
             </div>
           </Animate>
         )}
---
+++ src/model/Influencer.ts
@@ -0,0 +1,4 @@
+export interface Influencer {
+  name: string
+  socialNetwork: string
+}
