1c984763b58db7dc3687958e17f01533b025740a
Said SEDOUD <s.sedoud.betagouv@gmail.com>
2024-02-24 10:49:48 +0100

    TRELLO-2200: litige telecom page

---
+++ src/app/[lang]/remboursement-telecom/page.tsx
@@ -0,0 +1,6 @@
+import {buildGenerateMetadata} from '@/core/metadatas'
+import {RefundTelecom} from '@/reusablePages/refundTelecom'
+
+export const generateMetadata = buildGenerateMetadata('refundTelecom')
+
+export default RefundTelecom
