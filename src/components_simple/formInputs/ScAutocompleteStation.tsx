05b01008de30fe8e7288feffd5cfb1aa554495ed
Charles Dufour <charles.dufour@beta.gouv.fr>
2024-03-25 09:56:28 +0100

    [TRELLO-2225] Refacto

--- src/components_simple/formInputs/ScAutocompleteStation.tsx
+++ src/components_simple/formInputs/ScAutocompleteStation.tsx
@@ -1,21 +1,7 @@
-import {useEffect, useState} from 'react'
-import {useThrottle} from '@/utils/useThrottle'
 import {useApiClients} from '@/context/ApiClientsContext'
 import {useQuery} from '@tanstack/react-query'
 import {ScAutoComplete} from '@/components_simple/formInputs/ScAutocomplete'
-
-function useStateWithThrottledCopy<A>(defaultValue: A): [A, React.Dispatch<React.SetStateAction<A>>, A] {
-  const [state, setState] = useState(defaultValue)
-  const [throttledCopy, setThrottledCopy] = useThrottle(state, 5)
-  useEffect(() => {
-    // always copy the first state into the second one
-    // But since it's throttled, it will delay changes a bit
-    setThrottledCopy(state)
-  }, [state])
-  // Return a normal version (to be used when displaying)
-  // And a throttled copy (doesn't update as often, but it's useful to throttle API calls based on it)
-  return [state, setState, throttledCopy]
-}
+import {useStateWithThrottledCopy} from '@/hooks/useStateWithThrottledCopy'
 
 interface Props {
   label: string
