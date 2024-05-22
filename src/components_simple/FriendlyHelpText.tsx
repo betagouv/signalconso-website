3799d01c22592ef55ebfc262db5a4120f9093d24
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2024-05-22 13:32:14 +0200

    MobileAppPromoBanner is now more lightweight visually

--- src/components_simple/FriendlyHelpText.tsx
+++ src/components_simple/FriendlyHelpText.tsx
@@ -1,5 +1,5 @@
 import {ReactNode} from 'react'
 
-export function FriendlyHelpText({children}: {children: ReactNode}) {
-  return <div className="py-4 px-8 my-4 bg-sclightblueinfo text-scblueinfo">{children}</div>
+export function FriendlyHelpText({children, margins = true}: {children: ReactNode; margins?: boolean}) {
+  return <div className={`py-4 px-8 ${margins ? `my-4` : ''} bg-sclightblueinfo text-scblueinfo`}>{children}</div>
 }
