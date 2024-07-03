738b8d2a6d67c82522b45d65b3a441456a993885
Charles Dufour <charles.dufour@beta.gouv.fr>
2024-07-03 16:30:43 +0200

    [TRELLO-2439] Add comments

--- src/model/SpecialLegislation.ts
+++ src/model/SpecialLegislation.ts
@@ -8,6 +8,9 @@ export function appliedSpecialLegislation(
   company: CompanySearchResult,
   reportDraft: Pick<ReportDraft, 'tags'>,
 ): SpecialLegislation | undefined {
+  // 47.11C supérettes moins de 400 m2
+  // 47.11D supermarchés entre 400 et 2500
+  // 47.11F hypermarchés égale ou > à 2500 m2
   if (reportDraft.tags?.includes('Shrinkflation') && company.activityCode === '47.11C') {
     return 'SHRINKFLATION'
   }
