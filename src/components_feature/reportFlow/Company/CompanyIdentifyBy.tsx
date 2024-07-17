118b006e559ef6b315c0205016ae0e1c936e049e
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2024-07-17 13:11:41 +0300

    refacto: minor renaming/refacto in CompanyIdentifyBy

--- src/components_feature/reportFlow/Company/CompanyIdentifyBy.tsx
+++ src/components_feature/reportFlow/Company/CompanyIdentifyBy.tsx
@@ -1,9 +1,9 @@
 import {Animate} from '@/components_simple/Animate'
+import {ScAlert} from '@/components_simple/ScAlert'
 import {useI18n} from '@/i18n/I18n'
 import {ReactNode, useState} from 'react'
 import {CompanyKinds} from '../../../anomalies/Anomaly'
 import {ScRadioButtons} from '../../../components_simple/formInputs/ScRadioButtons'
-import {ScAlert} from '@/components_simple/ScAlert'
 
 export enum IdentifyBy {
   NAME_AND_POSTAL_CODE = 'NAME_AND_POSTAL_CODE',
@@ -17,30 +17,42 @@ interface Props {
   children: (identifyBy: IdentifyBy) => ReactNode
 }
 
-const isDistance = (companyKind: CompanyKinds) =>
-  companyKind === 'WEBSITE' ||
-  companyKind === 'MERCHANT_WEBSITE' ||
-  companyKind === 'TRANSPORTER_WEBSITE' ||
-  companyKind === 'PHONE'
-
+function isRemote(companyKind: CompanyKinds) {
+  return (
+    companyKind === 'WEBSITE' ||
+    companyKind === 'MERCHANT_WEBSITE' ||
+    companyKind === 'TRANSPORTER_WEBSITE' ||
+    companyKind === 'PHONE'
+  )
+}
 export const CompanyIdentifyBy = ({companyKind, children}: Props) => {
   const {m} = useI18n()
   const [identifyBy, setIdentifyBy] = useState<IdentifyBy | undefined>()
 
-  const createOptions = (companyKind: CompanyKinds) => [
-    ...(isDistance(companyKind)
-      ? [{label: m.identifyBy_name, description: m.identifyBy_nameDesc, value: IdentifyBy.NAME}]
-      : [{label: m.identifyBy_name_postal_code, description: m.identifyBy_nameDesc, value: IdentifyBy.NAME_AND_POSTAL_CODE}]),
-    {label: m.identifyBy_identity, description: m.identifyBy_identityDesc, value: IdentifyBy.IDENTITY},
-    ...(companyKind !== 'SIRET'
-      ? [
-          {
-            label: m.identifyBy_none,
-            description: m.identifyBy_noneDesc,
-            value: IdentifyBy.NONE,
-          },
-        ]
-      : []),
+  const optionName = {
+    label: m.identifyBy_name,
+    description: m.identifyBy_nameDesc,
+    value: IdentifyBy.NAME,
+  }
+  const optionNameAndCp = {
+    label: m.identifyBy_name_postal_code,
+    description: m.identifyBy_nameDesc,
+    value: IdentifyBy.NAME_AND_POSTAL_CODE,
+  }
+  const optionIdentity = {
+    label: m.identifyBy_identity,
+    description: m.identifyBy_identityDesc,
+    value: IdentifyBy.IDENTITY,
+  }
+  const optionNone = {
+    label: m.identifyBy_none,
+    description: m.identifyBy_noneDesc,
+    value: IdentifyBy.NONE,
+  }
+  const options = [
+    ...(isRemote(companyKind) ? [optionName] : [optionNameAndCp]),
+    optionIdentity,
+    ...(companyKind !== 'SIRET' ? [optionNone] : []),
   ]
 
   return (
@@ -59,7 +71,7 @@ export const CompanyIdentifyBy = ({companyKind, children}: Props) => {
             required
             value={identifyBy}
             onChange={setIdentifyBy}
-            options={createOptions(companyKind)}
+            options={options}
             title={m.canYouIdentifyCompany}
             titleNoAutoAsterisk
             description={m.canYouIdentifyCompanyDesc}
