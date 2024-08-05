bd513cb52d78a491fd8ebc401136792c7a292d58
SAID SEDOUD <said.sedoud.ext@geodis.com>
2024-08-05 17:15:35 +0200

    TRELLO-2487 : add loader on report submit

--- website/src/components_feature/reportFlow/reportFlowStepper/NextStepButton.tsx
+++ website/src/components_feature/reportFlow/reportFlowStepper/NextStepButton.tsx
@@ -2,6 +2,7 @@ import {useI18n} from '@/i18n/I18n'
 import {iconArrowRight} from '@/utils/utils'
 import {Button} from '@codegouvfr/react-dsfr/Button'
 import {StepNavigation} from './ReportFlowStepper'
+import {ButtonWithLoader} from "@/components_simple/buttons/Buttons";
 
 interface Props {
   loadingNext?: boolean
@@ -15,14 +16,15 @@ export const NextStepButton = ({nextButtonLabel, nextIconSend, loadingNext, onNe
   const {m} = useI18n()
   return (
     <div className="flex justify-end mt-6">
-      <Button
+      <ButtonWithLoader
         iconId={nextIconSend ? 'fr-icon-send-plane-line' : iconArrowRight}
         onClick={() => onNext(stepNavigation.next)}
         className="stepper-next-button"
         disabled={loadingNext === true}
+        loading={loadingNext === true}
       >
         {nextButtonLabel ?? m.next}
-      </Button>
+      </ButtonWithLoader>
     </div>
   )
 }
