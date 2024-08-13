788150de3cf3b1b11b6f7bba65b9399b5edab3bd
eletallbetagouv <107104509+eletallbetagouv@users.noreply.github.com>
2024-08-13 17:21:15 +0200

    suite

--- website/src/components_feature/reportFlow/Company/NewCompanyIdentification/NewCompanyIdentification.tsx
+++ website/src/components_feature/reportFlow/Company/NewCompanyIdentification/NewCompanyIdentification.tsx
@@ -1,14 +1,17 @@
+import {Animate} from '@/components_simple/Animate'
 import {ButtonWithLoader} from '@/components_simple/buttons/Buttons'
 import {ScAutocompletePostcode} from '@/components_simple/formInputs/ScAutocompletePostcode'
 import {ScCheckbox} from '@/components_simple/formInputs/ScCheckbox'
 import {ScTextInput} from '@/components_simple/formInputs/ScTextInput'
 import {FriendlyHelpText} from '@/components_simple/FriendlyHelpText'
+import {getCompanyKind} from '@/feature/reportUtils'
 import {Report} from '@/model/Report'
 import {CommonCompanyIdentification} from '@/model/Step2Model'
 import Button from '@codegouvfr/react-dsfr/Button'
 import Link from 'next/link'
 import {useState} from 'react'
 import {PartialReport} from '../../ReportFlowContext'
+import {CompanyAskForeignDetails} from '../CompanyAskForeignDetails'
 
 export function NewCompanyIdentification({
   draft,
@@ -18,11 +21,11 @@ export function NewCompanyIdentification({
   onIdentification: (_: CommonCompanyIdentification) => void
 }) {
   const [geographicalRestriction, setGeographicalRestriction] = useState(false)
-  const [iCantFind, setICantFind] = useState(false)
-  const [isForeign, setIsForeign] = useState(false)
+  const [mode, setMode] = useState<'search' | 'cannotFind' | 'foreign'>('search')
+  const companyKind = getCompanyKind(draft)
   return (
     <div>
-      {!iCantFind && !isForeign && (
+      {
         <>
           <h2 className="fr-h6">Rechercher une entreprise</h2>
           <ScTextInput
@@ -64,89 +67,67 @@ export function NewCompanyIdentification({
               Je lance la recherche
             </ButtonWithLoader>
             <div className="flex flex-col">
-              <Button
-                onClick={() => {
-                  setICantFind(true)
-                }}
-                priority="tertiary no outline"
-                iconId="ri-arrow-right-line"
-              >
+              <Button onClick={() => setMode('cannotFind')} priority="tertiary no outline" iconId="ri-arrow-right-line">
                 Je ne trouve pas / ne connais pas l'entreprise...
               </Button>
-              <Button
-                onClick={() => {
-                  setIsForeign(true)
-                }}
-                priority="tertiary no outline"
-                iconId="ri-arrow-right-line"
-              >
+              <Button onClick={() => setMode('foreign')} priority="tertiary no outline" iconId="ri-arrow-right-line">
                 L'entreprise est à l'étranger
               </Button>
             </div>
           </div>
         </>
-      )}
-      {iCantFind && (
-        <FriendlyHelpText>
-          {/* TODO ajuster pour cas où on ne contacte pas */}
-          <p>
-            <strong>
-              SignalConso ne fonctionne bien que quand vous identifiez l'entreprise avec laquelle vous avez un différent
-            </strong>
-            . Cela nous permet de la contacter immédiatement.
-          </p>
-          <p>
-            Si vous avez besoin d'un moteur de recherche plus poussé, vous pouvez essayer{' '}
-            <Link href={'https://annuaire-entreprises.data.gouv.fr/'} target="_blank">
-              L'Annuaire des Entreprises
-            </Link>
-            .
-          </p>
-          <p>
-            Si vraiment vous ne pouvez pas identifier l'entreprise, vous pouvez continuer quand même votre signalement. Il sera
-            communiqué aux agents de la répression des fraudes, qui feront de leur mieux pour traiter votre signalement. Mais les
-            chances de succès diminuent grandement.
-          </p>
-          <div className="flex gap-4 justify-between">
-            <Button priority="secondary" iconId="ri-arrow-left-line" onClick={() => setICantFind(false)}>
-              Je vais chercher un peu plus
-            </Button>
-            <Button priority="secondary" iconId="ri-arrow-right-line" iconPosition="right">
-              Je ne peux vraiment pas identifier l'entreprise
-            </Button>
+      }
+      {mode === 'cannotFind' && (
+        <Animate autoScrollTo>
+          <div>
+            <FriendlyHelpText>
+              {/* TODO ajuster pour cas où on ne contacte pas */}
+              <p>
+                <strong>
+                  SignalConso ne fonctionne bien que quand vous identifiez l'entreprise avec laquelle vous avez un différent
+                </strong>
+                . Cela nous permet de la contacter immédiatement.
+              </p>
+              <p>
+                Si vous avez besoin d'un moteur de recherche plus poussé, vous pouvez essayer{' '}
+                <Link href={'https://annuaire-entreprises.data.gouv.fr/'} target="_blank">
+                  L'Annuaire des Entreprises
+                </Link>
+                .
+              </p>
+              <p>
+                Si vraiment vous ne pouvez pas identifier l'entreprise, vous pouvez continuer quand même votre signalement. Il
+                sera communiqué aux agents de la répression des fraudes, qui feront de leur mieux pour traiter votre signalement.
+                Mais les chances de succès diminuent grandement.
+              </p>
+              <div className="flex gap-4 justify-between">
+                <Button priority="secondary" iconId="ri-arrow-left-line" onClick={() => setMode('search')}>
+                  Je vais chercher un peu plus
+                </Button>
+                <Button priority="secondary" iconId="ri-arrow-right-line" iconPosition="right">
+                  Je ne peux vraiment pas identifier l'entreprise
+                </Button>
+              </div>
+            </FriendlyHelpText>
           </div>
-        </FriendlyHelpText>
+        </Animate>
       )}
-      {isForeign && (
-        <FriendlyHelpText>
-          {/* TODO ajuster pour cas où on ne contacte pas */}
-          <p>
-            <strong>
-              SignalConso ne fonctionne bien que quand vous identifiez l'entreprise avec laquelle vous avez un différent
-            </strong>
-            . Cela nous permet de la contacter immédiatement.
-          </p>
-          <p>
-            Si vous avez besoin d'un moteur de recherche plus poussé, vous pouvez essayer{' '}
-            <Link href={'https://annuaire-entreprises.data.gouv.fr/'} target="_blank">
-              L'Annuaire des Entreprises
-            </Link>
-            .
-          </p>
-          <p>
-            Si vraiment vous ne pouvez pas identifier l'entreprise, vous pouvez continuer quand même votre signalement. Il sera
-            communiqué aux agents de la répression des fraudes, qui feront de leur mieux pour traiter votre signalement. Mais les
-            chances de succès diminuent grandement.
-          </p>
-          <div className="flex gap-4 justify-between">
-            <Button priority="secondary" iconId="ri-arrow-left-line" onClick={() => setICantFind(false)}>
-              Je vais chercher un peu plus
-            </Button>
-            <Button priority="secondary" iconId="ri-arrow-right-line" iconPosition="right">
-              Je ne peux vraiment pas identifier l'entreprise
-            </Button>
+      {mode === 'foreign' && (
+        <Animate autoScrollTo>
+          <div>
+            <CompanyAskForeignDetails
+              {...{companyKind}}
+              onSubmit={({name, postalCode, country: {code}}) => {
+                onIdentification({
+                  kind: 'foreignCompany',
+                  companyName: name,
+                  companyCountryCode: code,
+                  consumerPostalCode: postalCode,
+                })
+              }}
+            />
           </div>
-        </FriendlyHelpText>
+        </Animate>
       )}
     </div>
   )
