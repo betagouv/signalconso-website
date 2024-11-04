74323231a0894df4704bc8ec979bed981199f7cf
Charles Dufour <charles.dufour@beta.gouv.fr>
2024-11-04 11:59:17 +0100

    [TRELLO-2657] Refacto 4 LPs (#944)
    * [TRELLO-2657] Refacto 4 LPs
    
    * [TRELLO-2657] Refacto 4 LPs

---
+++ website/src/app/[lang]/achat-site/page.tsx
@@ -0,0 +1,6 @@
+import {buildGenerateMetadata} from '@/core/metadatas'
+import {achatSite} from '@/reusablePages/manual_landings/achatSite'
+
+export const generateMetadata = buildGenerateMetadata('achatSite')
+
+export default achatSite
---
+++ website/src/app/[lang]/demarches-administratives/page.tsx
@@ -0,0 +1,6 @@
+import {buildGenerateMetadata} from '@/core/metadatas'
+import {demarchesAdministratives} from '@/reusablePages/manual_landings/demarchesAdministratives'
+
+export const generateMetadata = buildGenerateMetadata('demarchesAdministratives')
+
+export default demarchesAdministratives
