b48b6cbb53aa274395d44051df89fc94b3a8b9bf
Said SEDOUD <s.sedoud.betagouv@gmail.com>
2024-11-25 16:35:22 +0100

    fix config

--- mobile-tools/src/config/config.ts
+++ mobile-tools/src/config/config.ts
@@ -8,7 +8,7 @@ export const Config = {
   rappelConsoUrl: noTrailingSlash(process.env.RAPPELCONSO_URL ?? ''),
   fetchTimeout: parseInt(process.env.MOBILE_HELPERS_FETCH_TIMEOUT ?? '5000'),
   oneSignalUrl: process.env.ONE_SIGNAL_URL ?? '',
-  oneSignalEnablePush: process.env.ONE_SIGNAL_ENABLE_PUSH === 'true' ?? false,
+  oneSignalEnablePush: process.env.ONE_SIGNAL_ENABLE_PUSH === 'true',
   oneSignalAppId: process.env.ONE_SIGNAL_APP_ID ?? '',
   oneSignaleApiKey: process.env.ONE_SIGNAL_API_KEY ?? '',
   websiteUrl: process.env.WEBSITE_URL ?? '',
