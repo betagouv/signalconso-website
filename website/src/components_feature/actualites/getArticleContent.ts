844f3843995391c64023f75b64418f3d173a2c74
Charles Dufour <charles.dufour@beta.gouv.fr>
2025-01-03 13:41:13 +0100

    [TRELLO-2817] Article Produits de la pêche (#995)

--- website/src/components_feature/actualites/getArticleContent.ts
+++ website/src/components_feature/actualites/getArticleContent.ts
@@ -40,11 +40,14 @@ import {ArticleDecorationsDeNoel} from '@/components_feature/actualites/ArticleD
 import {ArticleInterdictionPlastiques} from '@/components_feature/actualites/ArticleInterdictionPlastiques'
 import {ArticleCondamnationIndexia} from '@/components_feature/actualites/ArticleCondamnationIndexia'
 import {ArticleTarificationDynamique} from '@/components_feature/actualites/ArticleTarificationDynamique'
+import {ArticleProduitsDeLaPeche} from '@/components_feature/actualites/ArticleProduitsDeLaPeche'
 
 // We can't put import these JSX directly into the "newsArticlesData"
 // it causes too much import problems in the sitemap generation script
 export function getArticleContent(slug: NewsArticle['slug']): () => JSX.Element {
   switch (slug) {
+    case 'produits-de-la-peche':
+      return ArticleProduitsDeLaPeche
     case 'tarification-dynamique':
       return ArticleTarificationDynamique
     case 'condamnation-indexia':
