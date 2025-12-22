6fcf43e93d8408ea5123e7b3d3b05f4f1c9df74d
Charles Dufour <charles.dufour@beta.gouv.fr>
2025-12-22 21:55:05 +0100

    [TRELLO-3163] Message d'alerte informant de la suppression de l'app sur les stores (#1113)

--- website/src/reusablePages/faireUnSignalementPage.tsx
+++ website/src/reusablePages/faireUnSignalementPage.tsx
@@ -3,6 +3,7 @@ import {ReactNode} from 'react'
 import {Anomaly} from 'shared/anomalies/Anomaly'
 import {ReportFlowStepper} from '@/components_feature/reportFlow/reportFlowStepper/ReportFlowStepper'
 import {WebviewEnvMarker} from '@/utils/WebviewEnvMarker'
+import {Alert} from '@codegouvfr/react-dsfr/Alert'
 
 export const FaireUnSignalementPage = ({anomaly, isWebView}: {anomaly: Anomaly; isWebView: boolean}) => {
   return (
@@ -15,6 +16,13 @@ export const FaireUnSignalementPage = ({anomaly, isWebView}: {anomaly: Anomaly; 
 function Container({isWebView, children}: {isWebView: boolean; children: ReactNode}) {
   return isWebView ? (
     <div className="max-w-[624px] px-4 mx-auto pb-4">
+      <Alert
+        title="L’application mobile SignalConso va bientôt être retirée des stores."
+        severity="warning"
+        description="Elle ne sera plus mise à jour.
+Pour continuer à utiliser le service, rendez-vous sur le site de SignalConso : signal.conso.gouv.fr"
+      />
+      <br />
       <WebviewEnvMarker />
       {children}
     </div>
