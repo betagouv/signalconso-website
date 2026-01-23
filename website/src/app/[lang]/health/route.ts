d2a357740f4542f96007bbb6f56dba029b14f123
Said SEDOUD <s.sedoud.betagouv@gmail.com>
2026-01-23 17:17:32 +0100

    TRELLO-3166 : adding failing healthcheck to validate reboot

--- website/src/app/[lang]/health/route.ts
+++ website/src/app/[lang]/health/route.ts
@@ -1,5 +1,12 @@
 import { NextResponse } from 'next/server'
 
+const START_TIME = Date.now()
+const FIVE_MINUTES = 5 * 60 * 1000 // 5 minutes in milliseconds
+
 export function GET() {
+  const currentTime = Date.now()
+  if (currentTime - START_TIME > FIVE_MINUTES) {
+    return new NextResponse('Service Unavailable', { status: 503 })
+  }
   return new NextResponse('OK', { status: 200 })
 }
