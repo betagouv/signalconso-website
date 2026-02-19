1f4a047397eeae88f9beded691b24a23468b387b
Charles Dufour <charles.dufour@beta.gouv.fr>
2026-02-19 13:20:02 +0100

    [TRELLO-3163] Add an env var to forbid users to use the mobile app (#1115)

--- website/src/reusablePages/faireUnSignalementPage.tsx
+++ website/src/reusablePages/faireUnSignalementPage.tsx
@@ -1,9 +1,11 @@
 import {LimitedWidthPageContainer} from '@/components_simple/PageContainers'
-import {ReactNode} from 'react'
+import React, {ReactNode} from 'react'
 import {Anomaly} from 'shared/anomalies/Anomaly'
 import {ReportFlowStepper} from '@/components_feature/reportFlow/reportFlowStepper/ReportFlowStepper'
 import {WebviewEnvMarker} from '@/utils/WebviewEnvMarker'
 import {Alert} from '@codegouvfr/react-dsfr/Alert'
+import {appConfig} from '@/core/appConfig'
+import {WebviewDiscontinuedAlert} from '@/components_simple/WebviewDiscontinuedAlert'
 
 export const FaireUnSignalementPage = ({anomaly, isWebView}: {anomaly: Anomaly; isWebView: boolean}) => {
   return (
@@ -16,15 +18,23 @@ export const FaireUnSignalementPage = ({anomaly, isWebView}: {anomaly: Anomaly; 
 function Container({isWebView, children}: {isWebView: boolean; children: ReactNode}) {
   return isWebView ? (
     <div className="max-w-[624px] px-4 mx-auto pb-4">
-      <Alert
-        title="L’application mobile SignalConso va bientôt être retirée des stores."
-        severity="warning"
-        description="Elle ne sera plus mise à jour.
-Pour continuer à utiliser le service, rendez-vous sur le site de SignalConso : signal.conso.gouv.fr"
-      />
-      <br />
       <WebviewEnvMarker />
-      {children}
+      {appConfig.decommissionWebview ? (
+        <>
+          <WebviewDiscontinuedAlert />
+        </>
+      ) : (
+        <>
+          <Alert
+            title="L’application mobile SignalConso va bientôt être retirée des stores."
+            severity="warning"
+            description="Elle ne sera plus mise à jour.
+Pour continuer à utiliser le service, rendez-vous sur le site de SignalConso : signal.conso.gouv.fr"
+          />
+          <br />
+          {children}
+        </>
+      )}
     </div>
   ) : (
     <LimitedWidthPageContainer>{children}</LimitedWidthPageContainer>
