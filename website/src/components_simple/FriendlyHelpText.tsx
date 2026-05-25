6f221c3436462a31757b76d4e9286b2210207419
Said SEDOUD <s.sedoud.betagouv@gmail.com>
2026-05-25 11:13:43 +0200

    TRELLO-3213 : problem tooltip accessibility

--- website/src/components_simple/FriendlyHelpText.tsx
+++ website/src/components_simple/FriendlyHelpText.tsx
@@ -1,5 +1,12 @@
 import {ReactNode} from 'react'
 
-export function FriendlyHelpText({children, margins = true}: {children: ReactNode; margins?: boolean}) {
-  return <div className={`py-4 px-8 ${margins ? `my-4` : ''} bg-sclightblueinfo text-scblueinfo space-y-2`}>{children}</div>
+export function FriendlyHelpText({children, margins = true, tabIndex}: {children: ReactNode; margins?: boolean; tabIndex?: number}) {
+  return (
+    <div
+      tabIndex={tabIndex}
+      className={`py-4 px-8 ${margins ? `my-4` : ''} bg-sclightblueinfo text-scblueinfo space-y-2 rounded-sm${tabIndex !== undefined ? ' focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-scbluefrance' : ''}`}
+    >
+      {children}
+    </div>
+  )
 }
